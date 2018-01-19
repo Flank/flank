4f7e0a70a34745055d88f9fb14cfab16c6a325e9
bootstraponline <matt@bootstraponline.com>
2018-01-19 00:04:44 -0500

    Document video recording issue

--- cloud_testing/kotlin_poc/src/main/kotlin/ftl/RunConfig.kt
+++ cloud_testing/kotlin_poc/src/main/kotlin/ftl/RunConfig.kt
@@ -3,6 +3,8 @@ package ftl
 data class RunConfig(
         val useOrchestrator: String = "USE_ORCHESTRATOR", // must be USE_ORCHESTRATOR or null
         val disablePerformanceMetrics: Boolean = true,
-        val disableVideoRecording: Boolean = true,
+        // Disabling video recording causes infrastructure failures because the AVD idles and disconnects.
+        // Until this is resolved, video recording must be enabled.
+        val disableVideoRecording: Boolean = false,
         val testTimeoutMinutes: Long = 5
 )
