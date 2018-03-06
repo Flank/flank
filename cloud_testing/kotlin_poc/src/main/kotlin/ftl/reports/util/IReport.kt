86ef52a527f83972d7f246bb2c846749634234cc
bootstraponline <matt@bootstraponline.com>
2018-03-05 21:31:53 -0500

    Fix reportPath detection

--- cloud_testing/kotlin_poc/src/main/kotlin/ftl/reports/util/IReport.kt
+++ cloud_testing/kotlin_poc/src/main/kotlin/ftl/reports/util/IReport.kt
@@ -1,6 +1,8 @@
 package ftl.reports.util
 
+import ftl.config.FtlConstants.localResultsDir
 import ftl.json.MatrixMap
+import java.io.File
 import java.nio.file.Paths
 
 interface IReport {
@@ -11,6 +13,9 @@ interface IReport {
     }
 
     fun reportPath(matrices: MatrixMap): String {
-        return Paths.get(matrices.runPath, reportName()).toString()
+        var parent = File(matrices.runPath)
+        if (!parent.exists()) parent = Paths.get(localResultsDir, parent.name).toFile()
+
+        return Paths.get(parent.toString(), reportName()).toString()
     }
 }
