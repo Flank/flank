ebf0c52c7c270ee2eb0413a605cc76aca5a46e82
bootstraponline <code@bootstraponline.com>
2018-05-16 13:57:38 -0400

    Testing v1 (#180)
    * Update testing_v1.json
    
    * Update cloud testing API using private JSON
    
    * Add gradle build system for testing project
    
    * Fix package path for testing API

--- cloud_testing/apis/update_api_json.sh
+++ cloud_testing/apis/update_api_json.sh
@@ -2,6 +2,9 @@
 
 # npm -g install sort-json
 
+# Note: API discovery JSON is out of date. Check the gcloud CLI repo for most recent JSON.
+# https://github.com/bootstraponline/gcloud_cli/blob/master/google-cloud-sdk/lib/googlecloudsdk/third_party/apis/testing_v1.json
+
 TOOL_RESULTS=toolresults_v1beta3.json
 rm "$TOOL_RESULTS"
 curl -o "$TOOL_RESULTS" https://www.googleapis.com/discovery/v1/apis/toolresults/v1beta3/rest
