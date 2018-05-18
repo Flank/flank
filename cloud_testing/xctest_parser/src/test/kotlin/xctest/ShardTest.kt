3446e281d279f7037a36abfa01c0e762b1c7ee4b
bootstraponline <matt@bootstraponline.com>
2018-05-18 19:25:13 -0400

    Add testing todo

--- cloud_testing/xctest_parser/src/test/kotlin/xctest/ShardTest.kt
+++ cloud_testing/xctest_parser/src/test/kotlin/xctest/ShardTest.kt
@@ -1,37 +1,11 @@
 package xctest
 
-import com.dd.plist.NSArray
-import com.dd.plist.NSDictionary
-import com.dd.plist.PropertyListParser
 import org.junit.Test
-import java.io.File
-
-
 
 class ShardTest {
 
     @Test
     fun shardWorks() {
-        val xctestrun = File("./src/test/kotlin/xctest/fixtures/EarlGreyExampleSwiftTests_iphoneos11.3-arm64.xctestrun").canonicalFile
-        if (!xctestrun.exists()) throw RuntimeException("$xctestrun doesn't exist")
-
-        val root = PropertyListParser.parse(xctestrun) as NSDictionary
-
-        // OnlyTestIdentifiers
-        // Test-Class-Name[/Test-Method-Name]
-        // Example/testExample
-        val onlyTestIdentifiers = "OnlyTestIdentifiers"
-        for (testTarget in root.allKeys()) {
-            val testDictionary = (root[testTarget] as NSDictionary)
-
-            val nsArray = NSArray(1)
-            nsArray.setValue(0, "Example/testExample")
-
-            testDictionary.remove(onlyTestIdentifiers)
-            testDictionary["OnlyTestIdentifiers"] = nsArray
-        }
-
-
-        PropertyListParser.saveAsXML(root, xctestrun)
+        // TODO: test this without committing a giant binary blob to the git repo
     }
 }
