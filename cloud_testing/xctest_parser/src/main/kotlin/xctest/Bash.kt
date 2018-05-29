195460be4fc0a6df2841da4f2483790b3128605b
bootstraponline <matt@bootstraponline.com>
2018-05-29 18:05:37 -0400

    Close stream. Add waitFor

--- cloud_testing/xctest_parser/src/main/kotlin/xctest/Bash.kt
+++ cloud_testing/xctest_parser/src/main/kotlin/xctest/Bash.kt
@@ -1,11 +1,17 @@
 package xctest
 
 import java.lang.ProcessBuilder.Redirect.PIPE
+import java.util.concurrent.TimeUnit
 
 object Bash {
 
     private fun Process.failed(): Boolean {
         return this.exitValue() != 0
+    }
+
+    private fun Process.output(): String {
+        // must close the stream via use
+        return this.inputStream.bufferedReader().use { it.readText().trim() }
     }
 
     fun execute(cmd: String): String {
@@ -16,8 +22,10 @@ object Bash {
                 .redirectError(PIPE)
                 .start()
 
+        process.waitFor(2, TimeUnit.MINUTES)
+
         if (process.failed()) throw RuntimeException("Command failed: $cmd")
 
-        return process.inputStream.bufferedReader().readText().trim()
+        return process.output()
     }
 }
