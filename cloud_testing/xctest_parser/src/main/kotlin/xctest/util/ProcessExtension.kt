c26fdbbf943af0c14dc4b76ca9f3da579af2f00e
bootstraponline <matt@bootstraponline.com>
2018-05-29 20:45:37 -0400

    Address code review comment

--- cloud_testing/xctest_parser/src/main/kotlin/xctest/util/ProcessExtension.kt
+++ cloud_testing/xctest_parser/src/main/kotlin/xctest/util/ProcessExtension.kt
@@ -1,6 +1,5 @@
 package xctest.util
 
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 data class ProcessResult(
@@ -12,18 +11,15 @@ fun Process.failed(): Boolean {
     return this.exitValue() != 0
 }
 
-private val timeoutMs = Duration.ofMinutes(10).toMillis()
-
 fun Process.waitForResult(): ProcessResult {
     val readInput = StreamReader(this.inputStream)
     readInput.start()
     val readError = StreamReader(this.errorStream)
     readError.start()
 
-    this.waitFor(timeoutMs, TimeUnit.MILLISECONDS)
-    readInput.join(timeoutMs)
-    readError.join(timeoutMs)
+    this.waitFor(10, TimeUnit.MINUTES)
+    readInput.join()
+    readError.join()
 
     return ProcessResult(stdout = readInput.text, stderr = readError.text)
 }
-
