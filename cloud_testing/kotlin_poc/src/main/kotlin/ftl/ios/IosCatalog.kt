002e51dbc154b004e949cf80b9b736b7464b0b95
bootstraponline <code@bootstraponline.com>
2018-05-30 11:00:34 -0400

    Add iOS beta detection (#202)
    * Add iOS beta detection
    
    * Update readme.md

--- cloud_testing/kotlin_poc/src/main/kotlin/ftl/ios/IosCatalog.kt
+++ cloud_testing/kotlin_poc/src/main/kotlin/ftl/ios/IosCatalog.kt
@@ -8,7 +8,17 @@ import ftl.gc.GcTesting
  * note:  500 Internal Server Error is returned on invalid model id/version
  **/
 object IosCatalog {
-    private val iosDeviceCatalog by lazy { GcTesting.get.testEnvironmentCatalog().get("ios").execute().iosDeviceCatalog }
+    private val iosDeviceCatalog by lazy {
+        try {
+            GcTesting.get.testEnvironmentCatalog().get("ios").execute().iosDeviceCatalog
+        } catch (e: java.lang.Exception) {
+            throw java.lang.RuntimeException("""
+Unable to access the test environment catalog. Firebase Test Lab for iOS is currently in beta.
+Request access for your project via the following form:
+  https://goo.gl/forms/wAxbiNEP2pxeIRG82
+If this project has already been granted access, please email ftl-ios-feedback@google.com for support.""", e)
+        }
+    }
     private val iosModelIds by lazy { iosDeviceCatalog.models.map { it.id } }
     private val iosVersionIds by lazy { iosDeviceCatalog.versions.map { it.id } }
 
