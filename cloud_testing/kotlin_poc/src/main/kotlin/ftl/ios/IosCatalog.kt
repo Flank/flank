2c377bb654c23710d806ffb1c03978b94cd16c8a
Andy VanWagoner <andy@instructure.com>
2018-06-18 12:31:04 -0600

    Remove IosCatalog model and version methods
    The new supported method is the only api we need for validating the device and
    version matrix.

--- cloud_testing/kotlin_poc/src/main/kotlin/ftl/ios/IosCatalog.kt
+++ cloud_testing/kotlin_poc/src/main/kotlin/ftl/ios/IosCatalog.kt
@@ -19,26 +19,6 @@ Request access for your project via the following form:
 If this project has already been granted access, please email ftl-ios-feedback@google.com for support.""", e)
         }
     }
-    private val iosModelIds by lazy {
-        iosDeviceCatalog.models
-                .filter { it.supportedVersionIds?.isNotEmpty() ?: false }
-                .map { it.id }
-    }
-    private val iosVersionIds by lazy { iosDeviceCatalog.versions.map { it.id } }
-
-    fun model(modelId: String): String {
-        if (!iosModelIds.contains(modelId)) {
-            throw RuntimeException("Invalid iOS modelId '$modelId'.\nValid models are: $iosModelIds")
-        }
-        return modelId
-    }
-
-    fun version(versionId: String): String {
-        if (!iosVersionIds.contains(versionId)) {
-            throw RuntimeException("Invalid iOS versionId '$versionId'.\nValid versions are: $iosVersionIds")
-        }
-        return versionId
-    }
 
     fun supported(modelId: String, versionId: String): Boolean {
         val model = iosDeviceCatalog.models.find { it.id == modelId }
--- cloud_testing/kotlin_poc/src/test/kotlin/ftl/ios/IosCatalogTest.kt
+++ cloud_testing/kotlin_poc/src/test/kotlin/ftl/ios/IosCatalogTest.kt
@@ -1,49 +1,16 @@
 package ftl.ios
 
-import junit.framework.Assert.assertEquals
-import org.junit.Rule
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.rules.ExpectedException
 
 class IosCatalogTest {
-
-    @get:Rule
-    var thrown = ExpectedException.none()!!
 
     // iOS API requires authorization. Bitrise job is run without credentials for security reasons.
     // Skip tests that require auth when running on bitrise.
     private val bitrise = System.getenv("BITRISE_IO") != null
 
     @Test
-    fun validatesModelAndVersion() {
-        if (bitrise) return
-
-        IosCatalog.model("iphone8")
-        IosCatalog.version("11.2")
-    }
-
-    @Test
-    fun rejectsInvalidModel() {
-        if (bitrise) return
-
-        thrown.expect(RuntimeException::class.java)
-        thrown.expectMessage("Invalid iOS modelId")
-
-        IosCatalog.model("iphone88")
-    }
-
-    @Test
-    fun rejectsInvalidVersion() {
-        if (bitrise) return
-
-        thrown.expect(RuntimeException::class.java)
-        thrown.expectMessage("Invalid iOS versionId")
-
-        IosCatalog.version("11.22")
-    }
-
-    @Test
-    fun validatesSupportedModelAndVersion() {
+    fun supported() {
         if (bitrise) return
 
         assertEquals(false, IosCatalog.supported("bogus", "11.2"))
