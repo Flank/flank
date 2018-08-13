708e97d01c5c8e32891fb766db86b95cd4cc46e9
Zac Lippard <zlippard@godaddy.com>
2018-08-13 09:56:01 -0700

    dex parser library version bump to 2.0.0

--- cloud_testing/kotlin_poc/build.gradle.kts
+++ cloud_testing/kotlin_poc/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     compile("com.google.apis:google-api-services-toolresults:v1beta3-rev374-1.23.0")
 
     // https://github.com/linkedin/dex-test-parser/releases
-    compile("com.linkedin.dextestparser:parser:1.1.0")
+    compile("com.linkedin.dextestparser:parser:2.0.0")
 
     // NOTE: iOS support isn't in the public artifact. Use testing jar generated from the private gcloud CLI json
     // https://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.google.apis%22%20AND%20a%3A%22google-api-services-testing%22
