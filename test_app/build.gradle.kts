372faf2c82a95dfed63c78bff7749a3f125f8524
bootstraponline <matt@bootstraponline.com>
2018-08-27 18:12:55 -0400

    Update test_app

--- test_app/build.gradle.kts
+++ test_app/build.gradle.kts
@@ -6,10 +6,11 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.0.1'
+        classpath("com.android.tools.build:gradle:3.2.0-rc02")
+        classpath(kotlin("gradle-plugin", version = "1.2.61"))
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -20,6 +21,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
 }
--- test_app/gradlew.bat
+++ test_app/gradlew.bat
@@ -1,1 +1,1 @@
-../gradle/gradlew.bat
+../gradlew.bat
