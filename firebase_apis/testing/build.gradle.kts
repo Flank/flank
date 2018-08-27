2e6e1a1ea294f72ad781262ef1831119ad74ab2f
bootstraponline <matt@bootstraponline.com>
2018-08-27 18:48:24 -0400

    Update build settings

--- firebase_apis/testing/build.gradle.kts
+++ firebase_apis/testing/build.gradle.kts
@@ -1,3 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,2 @@
 include(":flank")
 include(":firebase_apis:testing")
-
-// include(":testing")
-// project(":testing").projectDir = File(rootProject.projectDir, "../apis/testing")
