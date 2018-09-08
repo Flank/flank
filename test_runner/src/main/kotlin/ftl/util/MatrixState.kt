02fb13cee1fed66c2663977235eb2f29fada3b8e
bootstraponline <matt@bootstraponline.com>
2018-09-07 21:47:00 -0400

    Test MatrixState. Don't crash on unknown state

--- test_runner/src/main/kotlin/ftl/util/MatrixState.kt
+++ test_runner/src/main/kotlin/ftl/util/MatrixState.kt
@@ -1,38 +1,21 @@
 package ftl.util
 
 object MatrixState {
-    // https://github.com/bootstraponline/studio-google-cloud-testing/blob/203ed2890c27a8078cd1b8f7ae12cf77527f426b/firebase-testing/src/com/google/gct/testing/CloudResultsLoader.java#L237
+    // https://github.com/bootstraponline/gcloud_cli/blob/0752e88b155a417a18d244c242b4ab3fb9aa1c1f/google-cloud-sdk/lib/googlecloudsdk/third_party/apis/testing_v1.json#L171
     // see testing_v1.json TestMatrix state enum
 
-    private const val TEST_STATE_UNSPECIFIED = "TEST_STATE_UNSPECIFIED"
-    private const val VALIDATING = "VALIDATING"
-    private const val PENDING = "PENDING"
-    private const val RUNNING = "RUNNING"
+    const val VALIDATING = "VALIDATING"
+    const val PENDING = "PENDING"
+    const val RUNNING = "RUNNING"
     const val FINISHED = "FINISHED"
-    private const val ERROR = "ERROR"
-    private const val UNSUPPORTED_ENVIRONMENT = "UNSUPPORTED_ENVIRONMENT" // Incompatible device/OS combination
-    private const val INCOMPATIBLE_ENVIRONMENT =
-        "INCOMPATIBLE_ENVIRONMENT" // Application does not support the specified OS version
-    private const val INCOMPATIBLE_ARCHITECTURE =
-        "INCOMPATIBLE_ARCHITECTURE" // Application does not support the specified device architecture
-    private const val CANCELLED = "CANCELLED"
-    private const val INVALID = "INVALID" // The provided APK is invalid
 
     fun inProgress(state: String): Boolean {
         return when (state) {
-            CANCELLED -> false
-            TEST_STATE_UNSPECIFIED -> false
-            UNSUPPORTED_ENVIRONMENT -> false
             VALIDATING -> true
-            INCOMPATIBLE_ENVIRONMENT -> false
-            INVALID -> false
-            INCOMPATIBLE_ARCHITECTURE -> false
             PENDING -> true
-            ERROR -> false
             RUNNING -> true
-            FINISHED -> false
             else -> {
-                throw RuntimeException("Unknown state: $state")
+                false
             }
         }
     }
