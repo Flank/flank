d1d88c97ea0f71353576e6df995f2524f8271cac
bootstraponline <matt@bootstraponline.com>
2018-10-08 23:43:56 -0400

    Use 60s timeout on requests by default

--- test_runner/src/main/kotlin/ftl/config/FtlConstants.kt
+++ test_runner/src/main/kotlin/ftl/config/FtlConstants.kt
@@ -1,11 +1,12 @@
 package ftl.config
 
-import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.googleapis.testing.auth.oauth2.MockGoogleCredential
+import com.google.api.client.googleapis.util.Utils
+import com.google.api.client.http.HttpRequestInitializer
 import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.json.JsonFactory
 
 object FtlConstants {
     var useMock = false
@@ -18,7 +19,7 @@ object FtlConstants {
     const val matrixIdsFile = "matrix_ids.json"
     const val applicationName = "Flank"
     const val GCS_PREFIX = "gs://"
-    val JSON_FACTORY: JacksonFactory by lazy { JacksonFactory.getDefaultInstance() }
+    val JSON_FACTORY: JsonFactory by lazy { Utils.getDefaultJsonFactory() }
 
     val httpTransport: NetHttpTransport by lazy {
         try {
@@ -28,7 +29,7 @@ object FtlConstants {
         }
     }
 
-    val credential: Credential by lazy {
+    val credential: HttpRequestInitializer by lazy {
         try {
             if (useMock) {
                 return@lazy MockGoogleCredential.Builder()
@@ -36,12 +37,14 @@ object FtlConstants {
                     .build()
             }
 
-            val defaultCredential = GoogleCredential.getApplicationDefault()
             // Scope is required.
             // https://developers.google.com/identity/protocols/googlescopes
             // https://developers.google.com/identity/protocols/application-default-credentials
             // https://cloud.google.com/sdk/gcloud/reference/alpha/compute/instances/set-scopes
-            return@lazy defaultCredential.createScoped(listOf("https://www.googleapis.com/auth/cloud-platform"))
+            val defaultCredential = GoogleCredential.getApplicationDefault()
+                .createScoped(listOf("https://www.googleapis.com/auth/cloud-platform"))
+
+            return@lazy TimeoutHttpRequestInitializer(defaultCredential)
         } catch (e: Exception) {
             throw RuntimeException(e)
         }
