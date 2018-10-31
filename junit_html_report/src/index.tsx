a98246069f5569321451e9f6240324cb8538efda
bootstraponline <matt@bootstraponline.com>
2018-10-31 15:09:42 -0400

    Update to create-react-app v2.1.0

--- junit_html_report/src/index.tsx
+++ junit_html_report/src/index.tsx
@@ -3,11 +3,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
+// http://bit.ly/CRA-PWA
+serviceWorker.register();
