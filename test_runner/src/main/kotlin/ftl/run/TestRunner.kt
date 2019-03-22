32dcef1d0bef26ff8d865426fed843e0e071c9c9
bootstraponline <matt@bootstraponline.com>
2019-03-22 14:52:08 -0400

    Save artifacts to device root

--- test_runner/src/main/kotlin/ftl/run/TestRunner.kt
+++ test_runner/src/main/kotlin/ftl/run/TestRunner.kt
@@ -27,15 +27,15 @@ import ftl.util.ObjPath
 import ftl.util.StopWatch
 import ftl.util.Utils
 import ftl.util.Utils.fatalError
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 object TestRunner {
     private val gson = GsonBuilder().setPrettyPrinting().create()!!
@@ -196,27 +196,24 @@ object TestRunner {
         return MatrixMap(map, path)
     }
 
-    fun getDownloadPaths(args: IArgs, blobPath: String): Pair<Path, Path> {
+    fun getDownloadPaths(args: IArgs, blobPath: String): Path {
         val localDir = args.localResultDir
         val p = ObjPath.parse(blobPath)
 
-        // results/2019-03-22_15-39-20.400000_ESdl/shard_0/a/b.txt
+        // results/2019-03-22_15-39-20.400000_ESdl/shard_0/NexusLowRes-28-en-portrait/b.txt
         //
-        // blobPath = 2019-03-22_15-39-20.400000_ESdl/shard_0/a/b.txt
+        // blobPath = 2019-03-22_15-39-20.400000_ESdl/shard_0/NexusLowRes-28-en-portrait/b.txt
         // localDir = results
-        // afterObjName = /shard_0/a/b.txt
+        // afterObjName = /shard_0/NexusLowRes-28-en-portrait/b.txt
         // shardName = shard_0
         // fileName = b.txt
         // objName = 2019-03-22_15-39-20.400000_ESdl
 
+        // Store downloaded artifacts at device root.
         return if (args.useLocalResultDir()) {
-            // results/shard_0/iphone8-12.0-en-portrait/a/b.txt or results/shard_0/b.txt
-            Paths.get(localDir, p.afterObjName) to
-                    Paths.get(localDir, p.shardName, p.fileName)
+            Paths.get(localDir, p.shardName, p.deviceName, p.fileName)
         } else {
-            // results/2019-03-22_15-39-20.400000_ESdl/shard_0/a/b.txt or results/2019-03-22_15-39-20.400000_ESdl/shard_0/b.txt
-            Paths.get(localDir, blobPath) to
-                    Paths.get(localDir, p.objName, p.shardName, p.fileName)
+            Paths.get(localDir, p.objName, p.shardName, p.deviceName, p.fileName)
         }
     }
 
@@ -243,21 +240,13 @@ object TestRunner {
                         val blobPath = blob.blobId.name
                         val matched = artifactsList.any { blobPath.matches(it) }
                         if (matched) {
-                            val (downloadFile, fallbackFile) = getDownloadPaths(args, blobPath)
+                            val downloadFile = getDownloadPaths(args, blobPath)
 
                             print(".")
                             if (!downloadFile.toFile().exists()) {
                                 val parentFile = downloadFile.parent.toFile()
                                 parentFile.mkdirs()
-                                // Not all paths can be recreated successfully locally.
-                                // Most iOS artifacts fail to download using the original path.
-                                val failed = parentFile.exists().not()
-                                if (failed) {
-                                    fallbackFile.parent.toFile().mkdirs()
-                                    blob.downloadTo(fallbackFile)
-                                } else {
-                                    blob.downloadTo(downloadFile)
-                                }
+                                blob.downloadTo(downloadFile)
                             }
                         }
                     }
--- test_runner/src/main/kotlin/ftl/util/ObjPath.kt
+++ test_runner/src/main/kotlin/ftl/util/ObjPath.kt
@@ -6,7 +6,7 @@ data class ObjPath(
     val fileName: String,
     val objName: String,
     val shardName: String,
-    val afterObjName: String
+    val deviceName: String
 ) {
     companion object {
         fun parse(path: String): ObjPath {
@@ -15,13 +15,13 @@ data class ObjPath(
             val fileName = parsed.fileName.toString()
             val objName = parsed.getName(0).toString()
             val shardName = parsed.getName(1).toString()
-            val afterObjName = parsed.subpath(1, parsed.nameCount).toString()
+            val deviceName = parsed.getName(2).toString()
 
             return ObjPath(
                 fileName = fileName,
                 objName = objName,
                 shardName = shardName,
-                afterObjName = afterObjName
+                deviceName = deviceName
             )
         }
     }
