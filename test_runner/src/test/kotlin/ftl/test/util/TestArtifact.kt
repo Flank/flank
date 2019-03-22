7de30da170d0d0dbacbe885d148bc24e57b2c6f0
bootstraponline <matt@bootstraponline.com>
2019-03-22 15:10:52 -0400

    Fix tests

--- test_runner/src/test/kotlin/ftl/test/util/TestArtifact.kt
+++ test_runner/src/test/kotlin/ftl/test/util/TestArtifact.kt
@@ -114,7 +114,6 @@ object TestArtifact {
 
         val unzip = "unzip \"$zipPath\" -d \"${fixtures.path}\""
         Bash.execute(unzip)
-        File(zipPath).copyTo(File("${fixtures.path}/ios_earlgrey2.zip"))
     }
 }
 
