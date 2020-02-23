e0e85eaebcfd28d80afcf573601e96dd6d0f58d7
Pawel Pasterz <pasterzp@gmail.com>
2020-02-24 00:25:47 +0100

    Fix bug where flag 'disable-sharding' causes bad request to TL

--- test_runner/src/main/kotlin/ftl/args/ArgsHelper.kt
+++ test_runner/src/main/kotlin/ftl/args/ArgsHelper.kt
@@ -221,14 +221,15 @@ object ArgsHelper {
     }
 
     fun calculateShards(filteredTests: List<String>, args: IArgs): List<List<String>> {
-        val oldTestResult = GcStorage.downloadJunitXml(args) ?: JUnitTestResult(mutableListOf())
+        val shards = if (args.disableSharding) {
+            mutableListOf(filteredTests as MutableList<String>)
+        } else {
+            val oldTestResult = GcStorage.downloadJunitXml(args) ?: JUnitTestResult(mutableListOf())
+            val shardCount = Shard.shardCountByTime(filteredTests, oldTestResult, args)
+            Shard.createShardsByShardCount(filteredTests, oldTestResult, args, shardCount).stringShards()
+        }
 
-        val shardCount = Shard.shardCountByTime(filteredTests, oldTestResult, args)
-
-        // TODO: manual sharding must be limited to 1..50 tests per shard
-        val shards = Shard.createShardsByShardCount(filteredTests, oldTestResult, args, shardCount)
-
-        return testMethodsAlwaysRun(shards.stringShards(), args)
+        return testMethodsAlwaysRun(shards, args)
     }
 
     private fun testMethodsAlwaysRun(shards: StringShards, args: IArgs): StringShards {
