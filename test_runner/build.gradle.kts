b2a19616ecfe47bdbe3c9111ddf8057941a4cdad
Sridhar Vadlamani <v.sridhar.sreenivas@gmail.com>
2020-02-25 01:45:24 -0800

    f

--- test_runner/build.gradle.kts
+++ test_runner/build.gradle.kts
@@ -128,79 +128,49 @@ tasks.withType<Test> {
 }
 
 dependencies {
+    implementation(Libs.BUGSNAG)
+
+    implementation(Libs.DD_PLIST)
+    implementation(Libs.DEX_TEST_PARSER)
+
+    implementation(Libs.GSON)
+
+    implementation(Libs.JACKSON_DATABIND)
+    implementation(Libs.JACKSON_KOTLIN)
+    implementation(Libs.JACKSON_YAML)
+    implementation(Libs.JACKSON_XML)
+
+    implementation(Libs.GOOGLE_NIO)
+    implementation(Libs.GOOGLE_AUTH)
+    implementation(Libs.GOOGLE_STORAGE)
+    implementation(Libs.GOOGLE_TOOLRESULTS)
+
+    implementation(Libs.KTOR_SERVER_CORE)
+    implementation(Libs.KTOR_SERVER_NETTY)
+    implementation(Libs.KTOR_GSON)
+
     implementation(Libs.KOTLIN_STD_LIB)
     implementation(Libs.KOTLIN_COROUTINES_CORE)
 
-    implementation("com.bugsnag:bugsnag:3.+")
+    implementation(Libs.LOGBACK)
 
-    // https://github.com/remkop/picocli/releases
-    implementation("info.picocli:picocli:4.0.4")
+    implementation(Libs.PICOCLI)
 
-    // https://github.com/google/gson/releases
-    implementation("com.google.code.gson:gson:2.8.6")
-
-    // https://github.com/3breadt/dd-plist/releases
-    implementation("com.googlecode.plist:dd-plist:1.22")
-
-    // https://search.maven.org/search?q=a:google-cloud-storage%20g:com.google.cloud
-    implementation("com.google.cloud:google-cloud-storage:1.96.0")
-
-    // https://search.maven.org/search?q=a:google-api-services-toolresults%20g:com.google.apis
-    implementation("com.google.apis:google-api-services-toolresults:v1beta3-rev20190923-1.30.3")
-
-    // https://github.com/linkedin/dex-test-parser/releases
-    implementation("com.linkedin.dextestparser:parser:2.1.1")
+    implementation(Libs.WOODSTOX)
 
     // NOTE: iOS support isn't in the public artifact. Use testing jar generated from the private gcloud CLI json
     // https://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.google.apis%22%20AND%20a%3A%22google-api-services-testing%22
     // compile("com.google.apis:google-api-services-testing:v1-rev30-1.23.0")
     implementation(project("test_api"))
 
-    // yaml config
-    // https://search.maven.org/search?q=a:jackson-databind%20g:com.fasterxml.jackson.core
-    implementation("com.fasterxml.jackson.core:jackson-databind:${Versions.JACKSON}")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.JACKSON}")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${Versions.JACKSON}")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:${Versions.JACKSON}")
+    detektPlugins(Libs.DETEKT_FORMATTING)
 
-    // https://github.com/FasterXML/jackson-dataformat-xml/releases
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:${Versions.JACKSON}")
-
-    // https://github.com/FasterXML/woodstox/releases
-    implementation("com.fasterxml.woodstox:woodstox-core:6.0.1")
-
-    // https://github.com/googleapis/google-auth-library-java/releases
-    // NOTE: https://github.com/googleapis/google-oauth-java-client is End of Life and replaced by google-auth-library-java
-    // https://github.com/googleapis/google-oauth-java-client/issues/251#issuecomment-504565533
-    implementation("com.google.auth:google-auth-library-oauth2-http:0.18.0")
-
-    // https://github.com/jhy/jsoup/releases
-    testImplementation("org.jsoup:jsoup:1.12.1")
+    testImplementation(Libs.JSOUP)
     testImplementation(Libs.JUNIT)
-    // https://github.com/google/truth/releases
-    testImplementation("com.google.truth:truth:1.0")
-    // https://github.com/stefanbirkner/system-rules/releases
-    testImplementation("com.github.stefanbirkner:system-rules:1.19.0")
-
-    // https://github.com/ktorio/ktor/releases
-    val ktorVersion = "1.2.5"
-    implementation("io.ktor:ktor-server-core:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("io.ktor:ktor-gson:$ktorVersion")
-
-    // https://github.com/qos-ch/logback/releases
-    implementation("ch.qos.logback:logback-classic:1.2.3")
-    // mockito-inline is used to mock final classes
-    // https://github.com/mockito/mockito/releases
-    testImplementation("org.mockito:mockito-inline:3.1.0")
-    // https://github.com/square/okhttp/releases
-    testImplementation("com.squareup.okhttp3:okhttp:4.2.2")
-
-    // todo: move to testImplementation once DI is implemented https://github.com/TestArmada/flank/issues/248
-    // https://search.maven.org/search?q=a:google-cloud-nio%20g:com.google.cloud
-    implementation("com.google.cloud:google-cloud-nio:0.114.0-alpha")
-
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${Versions.DETEKT}")
+    testImplementation(Libs.MOCKITO_INLINE)
+    testImplementation(Libs.OKHTTP)
+    testImplementation(Libs.SYSTEM_RULES)
+    testImplementation(Libs.TRUTH)
 }
 
 // Fix Exception in thread "main" java.lang.NoSuchMethodError: com.google.common.hash.Hashing.crc32c()Lcom/google/common/hash/HashFunction;
