d23d9107e16611e2960f76262526a0a11fe9b2bd
Janek Góral <jan.goral@gogoapps.io>
2020-02-25 14:19:57 +0100

    Remove test artifacts

--- test_app/bash/generate_apks.sh
+++ test_app/bash/generate_apks.sh
@@ -2,20 +2,18 @@
 
 set -euxo pipefail
 
+APK_OUTPUT=$1
+
 DIR=`dirname "$BASH_SOURCE"`
 
 TEST_APP="$DIR/.."
 
-APK="$DIR/apk"
-
 BUILD_OUTPUT="$TEST_APP/app/build/outputs/apk"
 
-mkdir $APK || true
-
-rm $APK/app-*.apk || true
+mkdir $APK_OUTPUT || true
 
 "$TEST_APP/gradlew" -p "$TEST_APP" clean assemble assembleAndroidTest
 
-cp $BUILD_OUTPUT/singleSuccess/debug/app-single-success-debug.apk $APK/app-debug.apk
+cp $BUILD_OUTPUT/singleSuccess/debug/app-single-success-debug.apk $APK_OUTPUT/app-debug.apk
 
-cp $BUILD_OUTPUT/androidTest/**/debug/*.apk $APK/
+cp $BUILD_OUTPUT/androidTest/**/debug/*.apk $APK_OUTPUT/
