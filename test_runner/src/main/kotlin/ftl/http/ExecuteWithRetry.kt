28dde1211fe4d02eac79a822b12e82092687e12c
Pawel Pasterz <paterzp@gmail.com>
2020-04-06 22:35:45 +0200

    Add exponential backoff

--- test_runner/src/main/kotlin/ftl/http/ExecuteWithRetry.kt
+++ test_runner/src/main/kotlin/ftl/http/ExecuteWithRetry.kt
@@ -1,18 +1,30 @@
 package ftl.http
 
 import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest
+import com.google.api.client.http.HttpResponseException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import java.io.IOException
+import kotlin.math.exp
+import kotlin.math.roundToInt
 
 // Only use on calls that don't change state.
 // Fetching status is safe to retry on timeout. Creating a matrix is not.
-fun <T> AbstractGoogleJsonClientRequest<T>.executeWithRetry(): T {
+fun <T> AbstractGoogleJsonClientRequest<T>.executeWithRetry(): T = withRetry { this.execute() }
+
+private inline fun <T> withRetry(crossinline block: () -> T): T = runBlocking {
     var lastErr: IOException? = null
 
-    repeat(10) {
+    repeat(4) {
         try {
-            return this.execute()
+            return@runBlocking block()
         } catch (err: IOException) {
             lastErr = err
+            // HttpStatusCodes from google api client does not have 429 code
+            if (err is HttpResponseException && err.statusCode == 429) {
+                return@repeat
+            }
+            delay(exp(it - 1.0).roundToInt().toLong())
         }
     }
 
