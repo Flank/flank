8efa2e30dd976390cb008411cb95635b2a096744
Filip Moroz <filip.moroz@gogoapps.io>
2020-04-22 11:33:53 +0200

    replace React app with plain TS

---
+++ junit_html_report/src/components/group.ts
@@ -0,0 +1,42 @@
+import {IResult, IResultGroup} from 'types';
+import {createElement} from 'helpers';
+
+const chevron = '<svg><use xlink:href="#icon-chevron"></use></svg>';
+
+function handleGroupClick(ev: MouseEvent) {
+  ev.preventDefault();
+
+  const target = ev.currentTarget as HTMLDivElement;
+
+  target.parentElement!.classList.toggle('isOpen');
+}
+
+function createGroupLabel(group: IResultGroup) {
+  return createElement(
+    'a',
+    `${chevron}<span>${group.label} (${group.items.length})</span>`,
+    { href: '#' },
+    { click: handleGroupClick },
+  );
+}
+
+function createGroupItem(item: IResult) {
+  return createElement(
+    'li',
+    `<a target="_blank" href="${item.url}">${item.label}</a>`,
+  );
+}
+
+export function createGroupNode(group: IResultGroup) {
+  const groupLabel = createGroupLabel(group);
+  const groupItemList = createElement(
+    'ul',
+    group.items.map(createGroupItem),
+  );
+
+  return createElement(
+    'li',
+    [groupLabel, groupItemList],
+    { class: 'group' },
+  );
+}
