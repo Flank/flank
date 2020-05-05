5b3529759441998591d0b8cda02237673e14001d
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-05-05 20:14:58 +0200

    Add spinner

--- test_runner/src/main/kotlin/ftl/cli/hypershard/android/HyperAndroidCommand.kt
+++ test_runner/src/main/kotlin/ftl/cli/hypershard/android/HyperAndroidCommand.kt
@@ -3,9 +3,15 @@ package ftl.cli.hypershard.android
 import com.dropbox.mobile.hypershard.ClassAnnotationValue
 import com.dropbox.mobile.hypershard.RealHyperShard
 import ftl.util.FlankFatalError
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
 import java.io.File
+import java.lang.Runnable
 import java.nio.file.Paths
 
 private const val RESULT_FILE = "hypershard-android-results.txt"
@@ -31,16 +37,23 @@ class HyperAndroidCommand : Runnable {
     private val regex = """\.(?=[^.]*$)""".toRegex()
 
     override fun run() {
-        val annotationValue = annotationName?.let { ClassAnnotationValue.Present(it) } ?: ClassAnnotationValue.Empty
-        val resultPath = Paths.get(outputPath, RESULT_FILE).toAbsolutePath().toString()
-        val hyperShard = RealHyperShard(annotationValue, dirs ?: throw FlankFatalError("Missing --dirs parameter"))
-        val tests = hyperShard.gatherTests().map { it.replace(regex, "#") }
-        File(resultPath).bufferedWriter().use { out ->
-            tests.forEach {
-                out.write(it)
-                out.newLine()
+        runBlocking {
+            val annotationValue = annotationName?.let { ClassAnnotationValue.Present(it) } ?: ClassAnnotationValue.Empty
+            val resultPath = Paths.get(outputPath, RESULT_FILE).toAbsolutePath().toString()
+            val hyperShard = RealHyperShard(annotationValue, dirs ?: throw FlankFatalError("Missing --dirs parameter"))
+
+            val spinner = launchSpinner()
+            val tests = hyperShard.gatherTests().map { it.replace(regex, "#") }
+
+            File(resultPath).bufferedWriter().use { out ->
+                tests.forEach {
+                    out.write(it)
+                    out.newLine()
+                }
+                spinner.cancel()
+                println("\rDone!")
+                println("Results file writen to $resultPath")
             }
-            println("Results file writen to $resultPath")
         }
     }
 
@@ -70,4 +83,21 @@ class HyperAndroidCommand : Runnable {
         description = ["Location where result file will be written"]
     )
     var outputPath: String = ""
+}
+
+private fun <T> Sequence<T>.repeat() = sequence {
+    while (true) {
+        yieldAll(this@repeat)
+    }
+}
+
+private fun CoroutineScope.launchSpinner() = launch(Dispatchers.IO) {
+    print("Working:   ")
+    listOf("\u28f7", "\u28ef", "\u28df", "\u287f", "\u28bf", "\u28fb", "\u28fd", "\u28fe")
+        .asSequence()
+        .repeat()
+        .forEach {
+            print("\b\b$it ")
+            delay(250)
+        }
 }
