9387a48d4f9f00c46559de0f81994447794a1549
Adam <adam.filipowicz92@gmail.com>
2020-05-07 20:03:07 +0200

    Add documentation of magicial numers

--- test_runner/src/main/kotlin/ftl/reports/xml/preprocesor/XmlPreprocessor.kt
+++ test_runner/src/main/kotlin/ftl/reports/xml/preprocesor/XmlPreprocessor.kt
@@ -3,8 +3,11 @@ package ftl.reports.xml.preprocesor
 import org.apache.commons.text.StringEscapeUtils
 
 fun fixHtmlCodes(data: String): String {
-    val isoHtmlCodesToReplace = listOf(0x00..0x1F).union(listOf(0x7F..0x9F)).flatten()
-        .map { StringEscapeUtils.escapeXml11(it.toChar().toString()) }.filter { it.startsWith("&#") }
+    val isoHtmlCodesToReplace =
+        listOf(UtfControlChars.CONTROL_TOP_START.charValue..UtfControlChars.CONTROL_TOP_END.charValue).union(
+            listOf(UtfControlChars.CONTROL_BOTTOM_START.charValue..UtfControlChars.CONTROL_BOTTOM_END.charValue)
+        ).flatten()
+            .map { StringEscapeUtils.escapeXml11(it.toChar().toString()) }.filter { it.startsWith("&#") }
 
     var fixedStr = data
     for (isoControlCode in isoHtmlCodesToReplace) {
