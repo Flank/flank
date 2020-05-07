795f9e296b77a3edf8461ab05f09329fd2ab7b6e
Adam <adam.filipowicz92@gmail.com>
2020-05-07 20:48:42 +0200

    set name of UtfControlChars enum to UtfControlCharRanges

--- test_runner/src/main/kotlin/ftl/reports/xml/preprocesor/XmlPreprocessor.kt
+++ test_runner/src/main/kotlin/ftl/reports/xml/preprocesor/XmlPreprocessor.kt
@@ -4,8 +4,8 @@ import org.apache.commons.text.StringEscapeUtils
 
 fun fixHtmlCodes(data: String): String {
     val isoHtmlCodesToReplace =
-        listOf(UtfControlChars.CONTROL_TOP_START.charValue..UtfControlChars.CONTROL_TOP_END.charValue).union(
-            listOf(UtfControlChars.CONTROL_BOTTOM_START.charValue..UtfControlChars.CONTROL_BOTTOM_END.charValue)
+        listOf(UtfControlCharRanges.CONTROL_TOP_START.charValue..UtfControlCharRanges.CONTROL_TOP_END.charValue).union(
+            listOf(UtfControlCharRanges.CONTROL_BOTTOM_START.charValue..UtfControlCharRanges.CONTROL_BOTTOM_END.charValue)
         ).flatten()
             .map { StringEscapeUtils.escapeXml11(it.toChar().toString()) }.filter { it.startsWith("&#") }
 
