ee575ae651516cb178f3e1c1149e9954528efd96
Adam <adam.filipowicz92@gmail.com>
2020-05-07 20:48:42 +0200

    set name of UtfControlChars enum to UtfControlCharRanges

--- test_runner/src/main/kotlin/ftl/reports/xml/preprocesor/UtfControlCharRanges.kt
+++ test_runner/src/main/kotlin/ftl/reports/xml/preprocesor/UtfControlCharRanges.kt
@@ -5,7 +5,7 @@ package ftl.reports.xml.preprocesor
  *   and represents control chars. We need to avoid characters in ranges CONTROL_TOP_START..CONTROL_TOP_END and
  *   CONTROL_BOTTOM_START..CONTROL_BOTTOM_END because chars from that range escaped to html causing parsing errors.
  * */
-enum class UtfControlChars(val charValue: Int) {
+enum class UtfControlCharRanges(val charValue: Int) {
     CONTROL_TOP_START(0x00),
     CONTROL_TOP_END(0x1F),
     CONTROL_BOTTOM_START(0x7F),
