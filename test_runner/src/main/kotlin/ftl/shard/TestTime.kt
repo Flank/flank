54a5d172279a05977fa68eb0a752212f4964d2b7
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-05-21 17:20:39 +0200

    #707 Refactor Shared object by splitting it into smaller functions

--- test_runner/src/main/kotlin/ftl/shard/TestTime.kt
+++
@@ -1,9 +0,0 @@
-package ftl.shard
-
-import com.google.common.annotations.VisibleForTesting
-
-// When a test does not have previous results to reference, fall back to this run time.
-@VisibleForTesting
-const val DEFAULT_TEST_TIME_SEC = 120.0
-@VisibleForTesting
-const val IGNORE_TEST_TIME = 0.0
