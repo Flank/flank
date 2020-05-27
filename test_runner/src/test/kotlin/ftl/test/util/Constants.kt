0ca035764a0efb0c249e7c3fefa725b183bcbfae
Janek Góral <jan.goral@gogoapps.io>
2020-05-27 03:51:35 +0200

    Add test for dumpShards

--- test_runner/src/test/kotlin/ftl/test/util/Constants.kt
+++ test_runner/src/test/kotlin/ftl/test/util/Constants.kt
@@ -1,3 +1,6 @@
 package ftl.test.util
 
-val mixedConfigYaml = TestHelper.getPath("src/test/kotlin/ftl/fixtures/test_app_cases/flank-multiple-mixed.yml")
+import ftl.test.util.TestHelper.getPath
+
+val mixedConfigYaml = getPath("src/test/kotlin/ftl/fixtures/test_app_cases/flank-multiple-mixed.yml")
+val ios2ConfigYaml = getPath("src/test/kotlin/ftl/fixtures/flank2.ios.yml")
