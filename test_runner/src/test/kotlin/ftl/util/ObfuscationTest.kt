929339c8a6ef9f2e2ad9df97fc0c0f5d5c47eeb2
piotradamczyk5 <65554637+piotradamczyk5@users.noreply.github.com>
2020-06-05 13:18:20 +0200

    Update ObfuscationTest.kt

--- test_runner/src/test/kotlin/ftl/util/ObfuscationTest.kt
+++ test_runner/src/test/kotlin/ftl/util/ObfuscationTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 internal class ObfuscationTest {
 
     @Test
-    fun `Should obfuscate android like test method string`() {
+    fun `should obfuscate android like test method string`() {
         // given
         val testString = "com.flank.Super#Test"
 
@@ -19,7 +19,7 @@ internal class ObfuscationTest {
     }
 
     @Test
-    fun `Should obfuscate ios like test method string`() {
+    fun `should obfuscate ios like test method string`() {
         // given
         val testString = "Flank/SuperTest"
 
@@ -31,7 +31,7 @@ internal class ObfuscationTest {
     }
 
     @Test
-    fun `Should obfuscate android method name and assign unique symbols to package and method`() {
+    fun `should obfuscate android method name and assign unique symbols to package and method`() {
         // given
         val testMethods = listOf(
             "com.example.Class1#method1",
@@ -61,7 +61,7 @@ internal class ObfuscationTest {
     }
 
     @Test
-    fun `Should obfuscate ios method name and assign unique symbols to class and method`() {
+    fun `should obfuscate ios method name and assign unique symbols to class and method`() {
         // given
         val testMethods = listOf(
             "SampleIOS/test",
