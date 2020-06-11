1b737b85f7992c3b2b8a33bcbd550d37395b7660
Janek Góral <jan.goral@gogoapps.io>
2020-06-11 14:04:09 +0200

    Add espresso parameterized named test

---
+++ test_app/app/src/androidTestMultiple/java/com/example/test_app/parametrized/EspressoParametrizedClassParameterizedNamed.kt
@@ -0,0 +1,37 @@
+package com.example.test_app.parametrized
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import com.example.test_app.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class EspressoParametrizedClassParameterizedNamed(
+    private val textButton: String,
+    private val expectedText: String
+) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index}: {0} {1}")
+        fun data() = listOf(
+            arrayOf("toast", "toast"),
+            arrayOf("alert", "alert"),
+            arrayOf("exception", "exception")
+        )
+    }
+
+    @Rule
+    @JvmField
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun clickRightButton() {
+        Espresso.onView(ViewMatchers.withText(textButton))
+            .check(ViewAssertions.matches(ViewMatchers.withText(expectedText)))
+    }
+}
