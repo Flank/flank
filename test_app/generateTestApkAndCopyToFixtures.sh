0d301051c24e58f629d2c7ed7a447846809ea027
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-06-18 14:51:49 +0200

    #818 fixed script

--- test_app/generateTestApkAndCopyToFixtures.sh
+++ test_app/generateTestApkAndCopyToFixtures.sh
@@ -1,4 +1,8 @@
 ./gradlew assembleAndroidTest
 
-mkdir -p ../test_runner/src/test/kotlin/ftl/fixtures/tmp/apks
-find . -type f -name "*androidTest.apk" -exec cp {} ../test_runner/src/test/kotlin/ftl/fixtures/tmp/apks \;
+if [ "$1" != "" ]; then
+    path=/$1
+fi
+
+mkdir -p ../test_runner/src/test/kotlin/ftl/fixtures/tmp/apks$path
+find . -type f -name "*androidTest.apk" -not -name "*-error-debug-androidTest.apk" -exec cp {} ../test_runner/src/test/kotlin/ftl/fixtures/tmp/apks$path \;
