8494fc5d0b046701694ef611c2f110ed1ff19005
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-06-18 16:20:54 +0200

    clean up multi modules, changes scripts

--- test_app/generateTestApkAndCopyToFixtures.sh
+++
@@ -1,8 +0,0 @@
-./gradlew assembleAndroidTest
-
-if [ "$1" != "" ]; then
-    path=/$1
-fi
-
-mkdir -p ../test_runner/src/test/kotlin/ftl/fixtures/tmp/apks$path
-find . -type f -name "*androidTest.apk" -not -name "*-error-debug-androidTest.apk" -exec cp {} ../test_runner/src/test/kotlin/ftl/fixtures/tmp/apks$path \;
