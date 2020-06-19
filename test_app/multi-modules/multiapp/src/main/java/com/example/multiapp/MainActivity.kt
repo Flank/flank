3181d92f0ebc8f99756460e69e29006180968532
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-06-19 15:16:30 +0200

    #818 detekt settings

--- test_app/multi-modules/multiapp/src/main/java/com/example/multiapp/MainActivity.kt
+++ test_app/multi-modules/multiapp/src/main/java/com/example/multiapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
