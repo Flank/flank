193d9e2a8eb0eb9f6c855832646a8a1cb403c02a
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-06-19 14:49:46 +0200

    #823 update tests

--- test_runner/src/test/kotlin/ftl/run/DumpShardsKtTest.kt
+++ test_runner/src/test/kotlin/ftl/run/DumpShardsKtTest.kt
@@ -78,28 +78,42 @@ class DumpShardsKtTest {
         val notExpected = """
 {
   "matrix-0": {
-    "app": "$path/src/test/kotlin/ftl/fixtures/tmp/apk/app-debug.apk",
-    "test": "$path/src/test/kotlin/ftl/fixtures/tmp/apk/app-single-success-debug-androidTest.apk",
+    "app": "/Users/piotr/Projekty/gogo/flank/test_runner/src/test/kotlin/ftl/fixtures/tmp/apk/app-debug.apk",
+    "test": "/Users/piotr/Projekty/gogo/flank/test_runner/src/test/kotlin/ftl/fixtures/tmp/apk/app-single-success-debug-androidTest.apk",
     "shards": {
       "shard-0": [
         "class com.example.test_app.InstrumentedTest#test"
       ]
-    }
+    },
+    "junit-ignored": [
+      "class com.example.test_app.InstrumentedTest#ignoredTest",
+      "class com.example.test_app.InstrumentedTest#ignoredTest2"
+    ]
   },
   "matrix-1": {
-    "app": "$path/src/test/kotlin/ftl/fixtures/tmp/apk/app-debug.apk",
-    "test": "$path/src/test/kotlin/ftl/fixtures/tmp/apk/app-multiple-flaky-debug-androidTest.apk",
+    "app": "/Users/piotr/Projekty/gogo/flank/test_runner/src/test/kotlin/ftl/fixtures/tmp/apk/app-debug.apk",
+    "test": "/Users/piotr/Projekty/gogo/flank/test_runner/src/test/kotlin/ftl/fixtures/tmp/apk/app-multiple-flaky-debug-androidTest.apk",
     "shards": {
       "shard-0": [
         "class com.example.test_app.InstrumentedTest#test1",
-        "class com.example.test_app.InstrumentedTest#test2"
+        "class com.example.test_app.InstrumentedTest#test2",
+        "class com.example.test_app.ParameterizedTest",
+        "class com.example.test_app.parametrized.EspressoParametrizedClassParameterizedNamed"
       ],
       "shard-1": [
         "class com.example.test_app.InstrumentedTest#test0",
         "class com.example.test_app.bar.BarInstrumentedTest#testBar",
-        "class com.example.test_app.foo.FooInstrumentedTest#testFoo"
+        "class com.example.test_app.foo.FooInstrumentedTest#testFoo",
+        "class com.example.test_app.parametrized.EspressoParametrizedClassTestParameterized",
+        "class com.example.test_app.parametrized.EspressoParametrizedMethodTestJUnitParamsRunner"
       ]
-    }
+    },
+    "junit-ignored": [
+      "class com.example.test_app.InstrumentedTest#ignoredTest1",
+      "class com.example.test_app.InstrumentedTest#ignoredTest2",
+      "class com.example.test_app.bar.BarInstrumentedTest#ignoredTestBar",
+      "class com.example.test_app.foo.FooInstrumentedTest#ignoredTestFoo"
+    ]
   }
 }
         """.trimIndent()
