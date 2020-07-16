0ee9b2b9e3b3e06b291bd5f9e24afdb47085d0a6
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-07-16 15:42:55 +0200

    #835 Fixed PR comments

--- test_runner/src/test/kotlin/ftl/cli/firebase/test/android/models/AndroidModelsListCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/firebase/test/android/models/AndroidModelsListCommandTest.kt
@@ -2,42 +2,10 @@ package ftl.cli.firebase.test.android.models
 
 import com.google.common.truth.Truth.assertThat
 import ftl.config.FtlConstants
-import ftl.test.util.TestHelper.normalizeLineEnding
-import org.junit.Rule
 import org.junit.Test
-import org.junit.contrib.java.lang.system.SystemOutRule
 import picocli.CommandLine
 
 class AndroidModelsListCommandTest {
-
-    @Rule
-    @JvmField
-    val systemOutRule: SystemOutRule = SystemOutRule().enableLog().muteForSuccessfulTests()
-
-    @Test
-    fun androidModelsListCommandPrintsHelp() {
-        val modelsListCommand = AndroidModelsListCommand()
-        assertThat(modelsListCommand.usageHelpRequested).isFalse()
-        CommandLine(modelsListCommand).execute("-h")
-
-        val output = systemOutRule.log.normalizeLineEnding()
-        assertThat(output).startsWith(
-            "Print current list of devices available to test against\n" +
-                    "\n" +
-                    "list [-h] [-c=<configPath>]\n" +
-                    "\n" +
-                    "Description:\n" +
-                    "\n" +
-                    "Print current list of Android devices available to test against\n" +
-                    "\n" +
-                    "Options:\n" +
-                    "  -c, --config=<configPath>\n" +
-                    "               YAML config file path\n" +
-                    "  -h, --help   Prints this help message"
-        )
-
-        assertThat(modelsListCommand.usageHelpRequested).isTrue()
-    }
 
     @Test
     fun androidModelsListCommandOptions() {
