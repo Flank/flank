2695fbc7fcee28ab116591793d665ef6bf7f5dc4
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-07-17 15:57:52 +0200

    #835 fixed PR comments

--- test_runner/src/test/kotlin/ftl/cli/firebase/test/android/versions/AndroidVersionsListCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/firebase/test/android/versions/AndroidVersionsListCommandTest.kt
@@ -1,23 +1,10 @@
 package ftl.cli.firebase.test.android.versions
 
 import com.google.common.truth.Truth.assertThat
-import ftl.config.FtlConstants
 import org.junit.Test
 import picocli.CommandLine
 
 class AndroidVersionsListCommandTest {
-
-    @Test
-    fun androidVersionsListCommandOptions() {
-        val cmd = AndroidVersionsListCommand()
-        assertThat(cmd.configPath).isEqualTo(FtlConstants.defaultAndroidConfig)
-        cmd.configPath = "tmp"
-        assertThat(cmd.configPath).isEqualTo("tmp")
-
-        assertThat(cmd.usageHelpRequested).isFalse()
-        cmd.usageHelpRequested = true
-        assertThat(cmd.usageHelpRequested).isTrue()
-    }
 
     @Test
     fun androidVersionsListCommandShouldParseConfig() {
