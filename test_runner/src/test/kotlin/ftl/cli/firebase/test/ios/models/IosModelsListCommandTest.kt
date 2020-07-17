5d7a298a04c00689b1cae5bf85e8d06636d9f15f
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-07-17 15:58:57 +0200

    #835 removed dummy tests

--- test_runner/src/test/kotlin/ftl/cli/firebase/test/ios/models/IosModelsListCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/firebase/test/ios/models/IosModelsListCommandTest.kt
@@ -1,23 +1,10 @@
 package ftl.cli.firebase.test.ios.models
 
 import com.google.common.truth.Truth.assertThat
-import ftl.config.FtlConstants
 import org.junit.Test
 import picocli.CommandLine
 
 class IosModelsListCommandTest {
-
-    @Test
-    fun iosModelsListCommandOptions() {
-        val cmd = IosModelsListCommand()
-        assertThat(cmd.configPath).isEqualTo(FtlConstants.defaultIosConfig)
-        cmd.configPath = "tmp"
-        assertThat(cmd.configPath).isEqualTo("tmp")
-
-        assertThat(cmd.usageHelpRequested).isFalse()
-        cmd.usageHelpRequested = true
-        assertThat(cmd.usageHelpRequested).isTrue()
-    }
 
     @Test
     fun iosModelsListCommandShouldParseConfig() {
--- test_runner/src/test/kotlin/ftl/cli/firebase/test/ios/versions/IosVersionsListCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/firebase/test/ios/versions/IosVersionsListCommandTest.kt
@@ -1,23 +1,10 @@
 package ftl.cli.firebase.test.ios.versions
 
 import com.google.common.truth.Truth.assertThat
-import ftl.config.FtlConstants
 import org.junit.Test
 import picocli.CommandLine
 
 class IosVersionsListCommandTest {
-
-    @Test
-    fun iosVersionsListCommandOptions() {
-        val cmd = IosVersionsListCommand()
-        assertThat(cmd.configPath).isEqualTo(FtlConstants.defaultIosConfig)
-        cmd.configPath = "tmp"
-        assertThat(cmd.configPath).isEqualTo("tmp")
-
-        assertThat(cmd.usageHelpRequested).isFalse()
-        cmd.usageHelpRequested = true
-        assertThat(cmd.usageHelpRequested).isTrue()
-    }
 
     @Test
     fun iosVersionsListCommandShouldParseConfig() {
