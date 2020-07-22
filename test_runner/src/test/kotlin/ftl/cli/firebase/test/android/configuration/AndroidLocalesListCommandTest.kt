74964489a5416437c79ea84e99d373beb4c44ffb
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-07-22 16:25:52 +0200

    #884 added printing available locales for Android devices

--- test_runner/src/test/kotlin/ftl/cli/firebase/test/android/configuration/AndroidLocalesListCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/firebase/test/android/configuration/AndroidLocalesListCommandTest.kt
@@ -1,10 +1,21 @@
 package ftl.cli.firebase.test.android.configuration
 
 import com.google.common.truth.Truth.assertThat
+import ftl.android.AndroidCatalog
+import io.mockk.mockkObject
+import io.mockk.verify
 import org.junit.Test
 import picocli.CommandLine
 
 class AndroidLocalesListCommandTest {
+
+    @Test
+    fun `should execute AndroidCatalog localesAsTable when run AndroidLocalesListCommand`() {
+        mockkObject(AndroidCatalog) {
+            CommandLine(AndroidLocalesListCommand()).execute()
+            verify { AndroidCatalog.localesAsTable(any()) }
+        }
+    }
 
     @Test
     fun androidLocalesListCommandShouldParseConfig() {
