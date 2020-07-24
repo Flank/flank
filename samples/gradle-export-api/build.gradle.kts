d591e978048501c3a2cc6f6f8c309a4c7a7a747b
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-07-24 12:50:51 +0200

    Add Gradle Enterprise API example

---
+++ samples/gradle-export-api/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    application
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "ftl.sample"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    implementation("com.squareup.okhttp3:okhttp:4.8.0")
+    implementation("com.squareup.okhttp3:okhttp-sse:4.8.0")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+application {
+    mainClassName = "ftl.sample.FetchBuildDataKt"
+}
---
+++ samples/gradle-export-api/src/main/kotlin/ftl/sample/FetchBuildData.kt
@@ -0,0 +1,61 @@
+package ftl.sample
+
+import okhttp3.Authenticator
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.Request
+import okhttp3.Route
+import okhttp3.sse.EventSource
+import okhttp3.sse.EventSourceListener
+import okhttp3.sse.EventSources.createFactory
+import java.time.Duration
+
+fun main() {
+    val id = ""
+    val password = ""
+    val baseUrl = "https://enterprise-training.gradle.com"
+    val url = "$baseUrl/build-export/v1/builds/since/now?stream"
+
+    val client = OkHttpClient.Builder()
+            .connectTimeout(Duration.ZERO)
+            .readTimeout(Duration.ZERO)
+            .authenticator(
+                    object : Authenticator {
+                        override fun authenticate(route: Route?, response: Response): Request? {
+                            return response.request
+                                    .newBuilder()
+                                    .addHeader("Authorization", Credentials.basic(id, password))
+                                    .build()
+                        }
+                    }
+            ).build()
+
+    val factory = createFactory(client)
+
+    val request = Request.Builder()
+            .url(url).build()
+    val listener = object : EventSourceListener() {
+        override fun onClosed(eventSource: EventSource) {
+            println("onClosed")
+            super.onClosed(eventSource)
+        }
+
+        override fun onEvent(eventSource: EventSource, id: String?, type: String?, data: String) {
+            println("onEvent $id $type $data")
+            super.onEvent(eventSource, id, type, data)
+        }
+
+        override fun onFailure(eventSource: EventSource, t: Throwable?, response: Response?) {
+            println("onFailure $t $response")
+            super.onFailure(eventSource, t, response)
+        }
+
+        override fun onOpen(eventSource: EventSource, response: Response) {
+            println("Start listening for build events\n")
+            println("onOpen $response")
+            super.onOpen(eventSource, response)
+        }
+    }
+    factory.newEventSource(request, listener)
+}
