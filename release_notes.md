0a030c79c10ff1c8d8c9b67d97cc351a5dc162bd
adamfilipow92 <64852261+adamfilipow92@users.noreply.github.com>
2020-08-11 11:17:48 +0200

    Fast fail when full-junit-result and legacy-junit-result  (#965)

--- release_notes.md
+++ release_notes.md
@@ -5,6 +5,7 @@
 - [#946](https://github.com/Flank/flank/pull/946) Added tests for flank scripts. ([piotradamczyk5](https://github.com/piotradamczyk5))
 - [#935](https://github.com/Flank/flank/pull/935) Process junit xml even when full junit is not enabled. ([kozaxinan](https://github.com/kozaxinan))
 - [#962](https://github.com/Flank/flank/pull/962) Make table text left aligned. ([pawelpasterz](https://github.com/pawelpasterz))
+- [#965](https://github.com/Flank/flank/pull/965) Fast fail when full-junit-result and legacy-junit-result. ([adamfilipow92](https://github.com/adamfilipow92))
 -
 -
 
--- test_runner/src/main/kotlin/ftl/args/ValidateAndroidArgs.kt
+++ test_runner/src/main/kotlin/ftl/args/ValidateAndroidArgs.kt
@@ -19,6 +19,7 @@ fun AndroidArgs.validate() {
     assertRoboTest()
     assertDirectoriesToPull()
     assertMaxTestShardsByDeviceType()
+    assertParametersConflict()
 }
 
 private fun AndroidArgs.assertAdditionalAppTestApks() {
@@ -98,4 +99,9 @@ private fun AndroidArgs.throwMaxTestShardsLimitExceeded(): Nothing {
     throw FlankConfigurationError(
         "max-test-shards must be >= ${IArgs.AVAILABLE_VIRTUAL_SHARD_COUNT_RANGE.first} and <= ${IArgs.AVAILABLE_VIRTUAL_SHARD_COUNT_RANGE.last} for virtual devices, for physical devices max-test-shards must be >= ${IArgs.AVAILABLE_PHYSICAL_SHARD_COUNT_RANGE.first} and <= ${IArgs.AVAILABLE_PHYSICAL_SHARD_COUNT_RANGE.last}, or -1. But current is $maxTestShards"
     )
+}
+
+private fun AndroidArgs.assertParametersConflict() {
+    if (useLegacyJUnitResult && fullJUnitResult)
+        throw FlankConfigurationError("Parameters conflict, you cannot set: `--legacy-junit-result` and `--full-junit-result` at the same time.")
 }
