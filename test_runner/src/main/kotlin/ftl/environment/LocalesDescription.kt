99039320c4d13a068cce240eb321cd7aacea4281
Adam <adam.filipowicz92@gmail.com>
2020-08-11 13:00:44 +0200

    Update LocalesDescription.kt

--- test_runner/src/main/kotlin/ftl/environment/LocalesDescription.kt
+++ test_runner/src/main/kotlin/ftl/environment/LocalesDescription.kt
@@ -9,6 +9,14 @@ private fun List<Locale>.findLocales(locale: String) = find { it.id == locale }
 private fun Locale.prepareDescription() = """
     id: $id
     name: $name
-""".trimIndent()
+""".trimIndent().addTagsIfExists(this)
+
+private fun String.addTagsIfExists(locale: Locale) =
+    if (locale.tags.isNullOrEmpty().not()) StringBuilder(this).appendln("\ntags:").appendTagsToList(locale).toString()
+    else this
+
+private fun StringBuilder.appendTagsToList(locale: Locale) = apply {
+    locale.tags.filterNotNull().forEach { tag -> appendln("- $tag") }
+}
 
 private fun String?.orErrorMessage(locale: String) = this ?: "ERROR: $locale is not a valid locale".trimIndent()
