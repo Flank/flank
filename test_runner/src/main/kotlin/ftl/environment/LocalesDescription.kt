afb622249a652b06cf3b8f69390a34a9a187da37
Adam <adam.filipowicz92@gmail.com>
2020-08-11 14:00:08 +0200

    Add locale describe tests

--- test_runner/src/main/kotlin/ftl/environment/LocalesDescription.kt
+++ test_runner/src/main/kotlin/ftl/environment/LocalesDescription.kt
@@ -2,9 +2,9 @@ package ftl.environment
 
 import com.google.api.services.testing.model.Locale
 
-fun List<Locale>.getLocaleDescription(locale: String) = findLocales(locale)?.prepareDescription().orErrorMessage(locale)
+fun List<Locale>.getLocaleDescription(localeId: String) = findLocales(localeId)?.prepareDescription().orErrorMessage(localeId)
 
-private fun List<Locale>.findLocales(locale: String) = find { it.id == locale }
+private fun List<Locale>.findLocales(localeId: String) = find { it.id == localeId }
 
 private fun Locale.prepareDescription() = """
     id: $id
@@ -12,11 +12,11 @@ private fun Locale.prepareDescription() = """
 """.trimIndent().addTagsIfExists(this)
 
 private fun String.addTagsIfExists(locale: Locale) =
-    if (locale.tags.isNullOrEmpty().not()) StringBuilder(this).appendln("\ntags:").appendTagsToList(locale).toString()
+    if (locale.tags.isNullOrEmpty().not()) StringBuilder(this).appendln("\ntags:").appendTagsToList(locale)
     else this
 
 private fun StringBuilder.appendTagsToList(locale: Locale) = apply {
     locale.tags.filterNotNull().forEach { tag -> appendln("- $tag") }
-}
+}.toString().trim()
 
 private fun String?.orErrorMessage(locale: String) = this ?: "ERROR: $locale is not a valid locale".trimIndent()
