7f77dc331b521b9c3ec1b11e5e9939f257d7cd6c
Adam <adam.filipowicz92@gmail.com>
2020-08-12 12:19:00 +0200

    Update LocalesDescription.kt

--- test_runner/src/main/kotlin/ftl/environment/LocalesDescription.kt
+++ test_runner/src/main/kotlin/ftl/environment/LocalesDescription.kt
@@ -9,14 +9,18 @@ private fun List<Locale>.findLocales(localeId: String) = find { it.id == localeI
 private fun Locale.prepareDescription() = """
     id: $id
     name: $name
-""".trimIndent().addTagsIfExists(this)
+""".trimIndent().addRegionIfExist(region).addTagsIfExists(this)
+
+private fun String.addRegionIfExist(region: String?) =
+    if (!region.isNullOrEmpty()) StringBuilder(this).appendln("\nregion: $region").toString()
+    else this
 
 private fun String.addTagsIfExists(locale: Locale) =
-    if (locale.tags.isNullOrEmpty().not()) StringBuilder(this).appendln("\ntags:").appendTagsToList(locale)
+    if (!locale.tags.isNullOrEmpty()) StringBuilder(this).appendln("\ntags:").appendTagsToList(locale)
     else this
 
 private fun StringBuilder.appendTagsToList(locale: Locale) = apply {
     locale.tags.filterNotNull().forEach { tag -> appendln("- $tag") }
 }.toString().trim()
 
-private fun String?.orErrorMessage(locale: String) = this ?: "ERROR: '$locale' is not a valid locale".trimIndent()
+private fun String?.orErrorMessage(locale: String) = this ?: "ERROR: '$locale' is not a valid locale"
