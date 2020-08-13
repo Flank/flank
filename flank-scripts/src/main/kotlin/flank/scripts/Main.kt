3625fef7b7691be992d176f16805c626c1bfe609
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-08-13 13:06:46 +0200

    split branch to 2

--- flank-scripts/src/main/kotlin/flank/scripts/Main.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/Main.kt
@@ -2,7 +2,6 @@ package flank.scripts
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
-import flank.scripts.ci.CiCommand
 import flank.scripts.release.ReleaseCommand
 
 class Main : CliktCommand(name = "flankScripts") {
@@ -11,7 +10,6 @@ class Main : CliktCommand(name = "flankScripts") {
 }
 
 fun main(args: Array<String>) {
-    Main()
-        .subcommands(ReleaseCommand(), CiCommand())
-        .main(args)
+    Main().subcommands(ReleaseCommand())
+            .main(args)
 }
