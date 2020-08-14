61f005d5a6d3a5fa6bf166916293196a0560e30f
Janek Góral <jan.goral@gogoapps.io>
2020-08-14 16:21:57 +0200

    Remove old implementation & clean

--- test_runner/src/main/kotlin/ftl/reports/outcome/CreateMatrixOutcomeSummary.kt
+++ test_runner/src/main/kotlin/ftl/reports/outcome/CreateMatrixOutcomeSummary.kt
@@ -3,15 +3,13 @@ package ftl.reports.outcome
 import com.google.api.services.toolresults.model.Environment
 
 fun TestOutcomeContext.createMatrixOutcomeSummary(): Pair<BillableMinutes, TestOutcome> =
-    steps.calculateAndroidBillableMinutes(projectId, testTimeout) to when {
-        environments.isNotEmpty() && environments.hasNoOutcome().not() ->
+    steps.calculateAndroidBillableMinutes(projectId, testTimeout) to
+        if (environments.isNotEmpty() && environments.hasNoOutcome().not())
             environments.createMatrixOutcomeSummaryUsingEnvironments(matrixId)
-
-        else -> {
+        else {
             if (steps.isEmpty()) println("No test results found, something went wrong. Try re-running the tests.")
             steps.createMatrixOutcomeSummaryUsingSteps(matrixId)
         }
-    }
 
 private fun List<Environment>.hasNoOutcome() = any { env ->
     env.environmentResult?.outcome?.summary == null
