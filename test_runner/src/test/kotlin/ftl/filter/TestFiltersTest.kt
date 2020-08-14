19ed45da4568b36d5e662d5e98da2c5fb5cff0dd
pgreze <pierrick.greze@gmail.com>
2020-08-14 16:12:29 +0900

    Update size samples to match all annotation generations

--- test_runner/src/test/kotlin/ftl/filter/TestFiltersTest.kt
+++ test_runner/src/test/kotlin/ftl/filter/TestFiltersTest.kt
@@ -23,9 +23,9 @@ val WITH_BAR_ANNOTATION = TestMethod("whatever.Foo#testName", listOf(TestAnnotat
 val WITHOUT_FOO_ANNOTATION = TestMethod("whatever.Foo#testName", emptyList())
 val WITH_FOO_ANNOTATION_AND_PACKAGE = TestMethod("foo.Bar#testName", listOf(TestAnnotation("Foo", emptyMap(), false)))
 val WITH_LARGE_ANNOTATION =
-    TestMethod("whatever.Foo#testName", listOf(TestAnnotation("androidx.test.filters.LargeTest", emptyMap(), false)))
+    TestMethod("whatever.Foo#testName", listOf(TestAnnotation("android.test.suitebuilder.annotation.LargeTest", emptyMap(), false)))
 val WITH_MEDIUM_ANNOTATION =
-    TestMethod("whatever.Foo#testName", listOf(TestAnnotation("MediumTest", emptyMap(), false)))
+    TestMethod("whatever.Foo#testName", listOf(TestAnnotation("android.support.test.filters.MediumTest", emptyMap(), false)))
 val WITH_SMALL_ANNOTATION =
     TestMethod("whatever.Foo#testName", listOf(TestAnnotation("androidx.test.filters.SmallTest", emptyMap(), false)))
 val WITHOUT_LARGE_ANNOTATION = TestMethod("whatever.Foo#testName", emptyList())
