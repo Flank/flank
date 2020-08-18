58356d83fa7c0d8a3b9cf40a0b20ae5300323e9c
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-08-18 22:50:08 +0200

    Refactor and make download async

--- test_runner/src/main/kotlin/ftl/run/common/FetchArtifacts.kt
+++ test_runner/src/main/kotlin/ftl/run/common/FetchArtifacts.kt
@@ -1,10 +1,12 @@
 package ftl.run.common
 
+import com.google.cloud.storage.Blob
 import com.google.cloud.storage.Storage
 import ftl.args.IArgs
 import ftl.config.FtlConstants
 import ftl.json.MatrixMap
 import ftl.gc.GcStorage
+import ftl.json.SavedMatrix
 import ftl.util.Artifacts
 import ftl.util.MatrixState
 import java.nio.file.Path
@@ -15,42 +17,33 @@ import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
 internal suspend fun fetchArtifacts(matrixMap: MatrixMap, args: IArgs) = coroutineScope {
-    println("FetchArtifacts")
-    val fields = Storage.BlobListOption.fields(Storage.BlobField.NAME)
-
+    val artifactsList = Artifacts.regexList(args)
     var dirty = false
-    val filtered = matrixMap.map.values.filter {
-        val finished = it.state == MatrixState.FINISHED
-        val notDownloaded = !it.downloaded
-        finished && notDownloaded
-    }
 
+    fun SavedMatrix.getBlobs() = Triple(
+        GcStorage.storage.list(
+            gcsRootBucket,
+            Storage.BlobListOption.prefix(gcsPathWithoutRootBucket),
+            Storage.BlobListOption.fields(Storage.BlobField.NAME)
+        ).iterateAll(), artifactsList, args
+    )
+
+    println("FetchArtifacts")
     print(FtlConstants.indent)
-    filtered.map { matrix ->
-        launch(Dispatchers.IO) {
-            val prefix = Storage.BlobListOption.prefix(matrix.gcsPathWithoutRootBucket)
-            val result = GcStorage.storage.list(matrix.gcsRootBucket, prefix, fields)
-            val artifactsList = Artifacts.regexList(args)
-
-            result.iterateAll().forEach { blob ->
-                val blobPath = blob.blobId.name
-                val matched = artifactsList.any { blobPath.matches(it) }
-                if (matched) {
-                    val downloadFile = getDownloadPath(args, blobPath)
-
-                    print(".")
-                    if (!downloadFile.toFile().exists()) {
-                        val parentFile = downloadFile.parent.toFile()
-                        parentFile.mkdirs()
-                        blob.downloadTo(downloadFile)
-                    }
-                }
-            }
-
-            dirty = true
-            matrix.downloaded = true
-        }
-    }.joinAll()
+    matrixMap
+        .onlyFinishedMatrices()
+        .onlyNotDownloadedMatrices()
+        .apply { if (isNotEmpty()) dirty = true }
+        .flatMap { savedMatrix ->
+            savedMatrix
+                .apply { downloaded = true }
+                .getBlobs()
+                .takeMatchedByArtifactList()
+                .getDownloadPaths()
+                .onlyNotExisting()
+                .prepareDirectories()
+                .downloadAsync()
+        }.joinAll()
     println()
 
     if (dirty) {
@@ -59,6 +52,27 @@ internal suspend fun fetchArtifacts(matrixMap: MatrixMap, args: IArgs) = corouti
         println()
     }
 }
+
+private fun MatrixMap.onlyFinishedMatrices() = map.values.filter { it.state == MatrixState.FINISHED }
+
+private fun List<SavedMatrix>.onlyNotDownloadedMatrices() = filterNot { it.downloaded }
+
+private fun Triple<MutableIterable<Blob>, List<Regex>, IArgs>.takeMatchedByArtifactList() = first
+    .asSequence()
+    .filter { blob -> second.any { blob.blobId.name.matches(it) } }
+    .map { it to third }
+
+private fun Sequence<Pair<Blob, IArgs>>.getDownloadPaths() = map { (blob, args) -> blob to getDownloadPath(args, blob.blobId.name) }
+
+private fun Sequence<Pair<Blob, Path>>.onlyNotExisting() = filterNot { (_, path) -> path.toFile().exists() }
+
+private fun Sequence<Pair<Blob, Path>>.prepareDirectories() = map { (blob, path) ->
+    print(".")
+    path.parent.toFile().mkdirs();
+    { blob.downloadTo(path) }
+}
+
+private suspend fun Sequence<() -> Unit>.downloadAsync() = coroutineScope { map { launch(Dispatchers.IO) { it() } }.toList() }
 
 internal fun getDownloadPath(args: IArgs, blobPath: String): Path {
     val localDir = args.localResultDir
