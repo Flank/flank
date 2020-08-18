9213c8d24db764f22e57cc5a1f29b780652e5f4e
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-08-18 13:55:24 +0200

    Generate release notes for GitHub release description

--- flank-scripts/src/main/kotlin/flank/scripts/release/hub/ReleaseFlank.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/release/hub/ReleaseFlank.kt
@@ -1,39 +1,47 @@
 package flank.scripts.release.hub
 
+import flank.scripts.ci.releasenotes.generateReleaseNotes
+import flank.scripts.utils.markdownH2
 import flank.scripts.utils.runCommand
 import java.io.File
 import java.nio.file.Path
 
-fun releaseFlank(path: Path, gitTag: String, commitHash: String, isSnapshotRelease: Boolean): Int {
+fun releaseFlank(path: Path, gitTag: String, commitHash: String, isSnapshotRelease: Boolean, token: String): Int {
     val releasePath = moveFlankToReleaseDirectory(path)
     val releaseCommand = if (isSnapshotRelease) {
         hubStableSnapshotCommand(releasePath, gitTag, commitHash)
     } else {
-        hubStableReleaseCommand(releasePath, gitTag, commitHash)
+        hubStableReleaseCommand(releasePath, gitTag, token)
     }
     return releaseCommand.runCommand()
 }
 
-private fun hubStableReleaseCommand(path: String, gitTag: String, commitHash: String) =
-        listOf(
-                "hub", "release", "create",
-                "-a", path,
-                "-m", "Flank $gitTag",
-                "-m", "Stable release for commit $commitHash",
-                gitTag
-        )
+private fun moveFlankToReleaseDirectory(inputPath: Path) =
+    if (inputPath.toFile().renameTo(File(RELEASE_DIRECTORY))) RELEASE_DIRECTORY else inputPath.toAbsolutePath().toString()
 
 private fun hubStableSnapshotCommand(path: String, gitTag: String, commitHash: String) =
-        listOf(
-                "hub", "release", "create",
-                "-p",
-                "-a", path,
-                "-m", "Flank $gitTag",
-                "-m", "Snapshot release for commit $commitHash",
-                gitTag
-        )
+    listOf(
+        "hub", "release", "create",
+        "-p",
+        "-a", path,
+        "-m", "Flank $gitTag",
+        "-m", "Snapshot release for commit $commitHash",
+        gitTag
+    )
 
-private fun moveFlankToReleaseDirectory(inputPath: Path) =
-        if (inputPath.toFile().renameTo(File(RELEASE_DIRECTORY))) RELEASE_DIRECTORY else inputPath.toAbsolutePath().toString()
+private fun hubStableReleaseCommand(path: String, gitTag: String, token: String) =
+    listOf(
+        "hub", "release", "create",
+        "-a", path,
+        "-m", "Flank $gitTag",
+        "-m", generateReleaseNotes(token).asReleaseBody(gitTag),
+        gitTag
+    )
+
+private fun List<String>.asReleaseBody(tag: String) =
+    StringBuilder(tag.markdownH2())
+        .appendln()
+        .apply { this@asReleaseBody.forEach { appendln(it) } }
+        .toString()
 
 private const val RELEASE_DIRECTORY = "./RELEASE/flank.jar"
--- flank-scripts/src/test/kotlin/flank/scripts/release/hub/ReleaseFlankCommandTest.kt
+++ flank-scripts/src/test/kotlin/flank/scripts/release/hub/ReleaseFlankCommandTest.kt
@@ -13,15 +13,46 @@ class ReleaseFlankCommandTest {
     val systemExit = ExpectedSystemExit.none()!!
 
     @Test
-    fun `Should return same exit code as command`() {
-        // given
-        mockkStatic("flank.scripts.release.hub.ReleaseFlankKt")
-        every { releaseFlank(any(), any(), any(), any()) } returns 1
-
+    fun `Should return with exit code 1 when snapshot and no commit hash specified`() {
         // expect
         systemExit.expectSystemExitWithStatus(1)
 
         // when
-        ReleaseFlankCommand().main(listOf("--input-file=./", "--git-tag=T", "--commit-hash=X"))
+        ReleaseFlankCommand().main(listOf("--input-file=./", "--git-tag=T", "--snapshot"))
+    }
+
+    @Test
+    fun `Should return with exit code 1 when not snapshot and no github token specified`() {
+        // expect
+        systemExit.expectSystemExitWithStatus(1)
+
+        // when
+        ReleaseFlankCommand().main(listOf("--input-file=./", "--git-tag=T"))
+    }
+
+    @Test
+    fun `Should return successfully run release for snasphot`() {
+        // given
+        mockkStatic("flank.scripts.release.hub.ReleaseFlankKt")
+        every { releaseFlank(any(), any(), any(), any(), any()) } returns 0
+
+        // expect
+        systemExit.expectSystemExitWithStatus(0)
+
+        // when
+        ReleaseFlankCommand().main(listOf("--input-file=./", "--git-tag=T", "--commit-hash=X", "--snapshot"))
+    }
+
+    @Test
+    fun `Should return successfully run release for stable`() {
+        // given
+        mockkStatic("flank.scripts.release.hub.ReleaseFlankKt")
+        every { releaseFlank(any(), any(), any(), any(), any()) } returns 0
+
+        // expect
+        systemExit.expectSystemExitWithStatus(0)
+
+        // when
+        ReleaseFlankCommand().main(listOf("--input-file=./", "--git-tag=T", "--token=X"))
     }
 }
