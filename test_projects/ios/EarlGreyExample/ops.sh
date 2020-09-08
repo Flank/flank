1fb72de8eda1cb3f8e2a18a70999e9063d3fd5ed
Janek Góral <jan.goral@gogoapps.io>
2020-09-08 12:44:26 +0200

    Update earl_grey_example function

--- test_projects/ios/EarlGreyExample/ops.sh
+++ test_projects/ios/EarlGreyExample/ops.sh
@@ -1,44 +1,71 @@
 #!/usr/bin/env bash
 
 function setup_ios_env() {
+  if ! [ -x "$(command -v xcpretty)" ]; then
+    gem install cocoapods
+  fi
   local dir=$(dirname "$BASH_SOURCE")
-  gem install cocoapods
   (cd "$dir" && pod install)
 }
 
-function earl_grey_example() {
-  local dir=$(dirname "$BASH_SOURCE")
-  for arg in "$@"; do case "$arg" in
-
-    '--generate' | '-g')
-      "$dir/build_example.sh"
-      ;;
-
-    '--copy' | '-c')
-      echo "TODO"
-      ;;
-
-    esac done
-}
-
-function earl_grey_ftl() {
-  local dir=$(dirname "$BASH_SOURCE")
-  for arg in "$@"; do case "$arg" in
-
-    '--generate' | '-g')
-      "$dir/build_ftl.sh"
-      ;;
-
-    '--copy' | '-c')
-      echo "--copy TODO"
-      ;;
-
-    esac done
+function install_xcpretty() {
+  if ! [ -x "$(command -v xcpretty)" ]; then
+    gem install xcpretty
+  fi
 }
 
 function universal_framework() {
   local dir=$(dirname "$BASH_SOURCE")
   "$dir/universal_framework.sh"
+}
+
+function earl_grey_example() {
+  local dir=$(dirname "$BASH_SOURCE")
+  local buildDir="$dir/build"
+
+  for arg in "$@"; do case "$arg" in
+
+    '--generate' | '-g')
+
+      install_xcpretty
+
+      rm -rf "$buildDir"
+
+      xcodebuild build-for-testing \
+        -allowProvisioningUpdates \
+        -workspace "$dir/EarlGreyExample.xcworkspace" \
+        -scheme "EarlGreyExampleSwiftTests" \
+        -derivedDataPath "$buildDir" \
+        -sdk iphoneos |
+        xcpretty
+
+      xcodebuild build-for-testing \
+        -allowProvisioningUpdates \
+        -workspace "$dir/EarlGreyExample.xcworkspace" \
+        -scheme "EarlGreyExampleTests" \
+        -derivedDataPath "$buildDir" \
+        -sdk iphoneos |
+        xcpretty
+      ;;
+
+    '--copy' | '-c')
+
+      local productsDir="$dir/build/Build/Products"
+
+      cp -R "$productsDir/*-iphoneos" "$FLANK_FIXTURES_TMP/"
+
+      cp "$productsDir/*.xctestrun" "$FLANK_FIXTURES_TMP/"
+
+      cp \
+        "$productsDir/Debug-iphoneos/EarlGreyExampleTests.xctest/EarlGreyExampleTests" \
+        "$FLANK_FIXTURES_TMP/objc/"
+
+      cp \
+        "$productsDir/Debug-iphoneos/EarlGreyExampleSwiftTests.xctest/EarlGreyExampleSwiftTests" \
+        "$FLANK_FIXTURES_TMP/swift/"
+      ;;
+
+    esac done
 }
 
 echo "iOS test projects ops loaded"
