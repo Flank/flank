2aa2ad333b7af52f7e224782b290fd288a03602f
pawelpasterz <32893017+pawelpasterz@users.noreply.github.com>
2020-09-08 09:49:48 +0200

    fix: Fix gcs files validation (#1100)
    * Skip validation for gcs files
    
    * Add comment
    
    * Update tests

--- test_runner/src/main/kotlin/ftl/args/ArgsHelper.kt
+++ test_runner/src/main/kotlin/ftl/args/ArgsHelper.kt
@@ -42,6 +42,10 @@ object ArgsHelper {
     }
 
     fun assertFileExists(file: String, name: String) {
+        // flank can't detect (for now) if files exists on GCS, therefore we want to skip validation
+        // todo https://github.com/Flank/flank/issues/1099
+        if (file.startsWith(GCS_PREFIX)) return
+
         if (!File(file).exists()) {
             throw FlankGeneralError("'$file' $name doesn't exist")
         }
