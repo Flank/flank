c29d1a9089f2b80eacb21584981018cba7b2a72d
Janek Góral <jan.goral@gogoapps.io>
2020-09-14 14:32:32 +0200

    Fix gradlew usage

--- flank-scripts/bash/buildFlankScripts.sh
+++ flank-scripts/bash/buildFlankScripts.sh
@@ -4,5 +4,5 @@ DIR=`dirname "$BASH_SOURCE"`
 
 FLANK_SCRIPTS="$DIR/.."
 
-"$FLANK_SCRIPTS/gradlew" -p "$FLANK_SCRIPTS" clean assemble shadowJar
+"$FLANK_SCRIPTS/../gradlew" flank-scripts:clean flank-scripts:assemble flank-scripts:shadowJar
 cp "$FLANK_SCRIPTS"/build/libs/flankScripts.jar "$DIR/flankScripts.jar"
