b78e3686451c81ef4c3ba990424eb63062a794e4
Adam <adam.filipowicz92@gmail.com>
2020-09-16 18:52:50 +0200

    Update AndroidTestConfig.kt

--- test_runner/src/main/kotlin/ftl/run/platform/android/AndroidTestConfig.kt
+++ test_runner/src/main/kotlin/ftl/run/platform/android/AndroidTestConfig.kt
@@ -5,21 +5,21 @@ import ftl.args.ShardChunks
 
 sealed class AndroidTestConfig {
     data class Instrumentation(
-        val appApkGcsPath: String,
-        val testApkGcsPath: String,
-        val testRunnerClass: String?,
-        val orchestratorOption: String?,
-        // sharding
-        val disableSharding: Boolean,
-        val testShards: ShardChunks,
-        val numUniformShards: Int?,
-        val keepTestTargetsEmpty: Boolean,
-        val environmentVariables: Map<String, String> = emptyMap()
+            val appApkGcsPath: String,
+            val testApkGcsPath: String,
+            val testRunnerClass: String?,
+            val orchestratorOption: String?,
+            // sharding
+            val disableSharding: Boolean,
+            val testShards: ShardChunks,
+            val numUniformShards: Int?,
+            val keepTestTargetsEmpty: Boolean,
+            val environmentVariables: Map<String, String> = emptyMap()
     ) : AndroidTestConfig()
 
     data class Robo(
-        val appApkGcsPath: String,
-        val flankRoboDirectives: List<FlankRoboDirective>?,
-        val roboScriptGcsPath: String?
+            val appApkGcsPath: String,
+            val flankRoboDirectives: List<FlankRoboDirective>?,
+            val roboScriptGcsPath: String?
     ) : AndroidTestConfig()
 }
