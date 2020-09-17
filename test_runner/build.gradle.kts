f4130db760c6793d720a040aa06fdf3e7e5e7089
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-09-17 21:54:08 +0200

    Fix dependencies

--- test_runner/build.gradle.kts
+++ test_runner/build.gradle.kts
@@ -252,16 +252,6 @@ dependencies {
     implementation(Dependencies.JANSI)
 }
 
-// Fix Exception in thread "main" java.lang.NoSuchMethodError: com.google.common.hash.Hashing.crc32c()Lcom/google/common/hash/HashFunction;
-// https://stackoverflow.com/a/45286710
-configurations.all {
-    resolutionStrategy {
-        force("com.google.guava:guava:25.1-jre")
-        force(Libs.KOTLIN_REFLECT)
-        exclude(group = "com.google.guava", module = "guava-jdk5")
-    }
-}
-
 buildscript {
     repositories {
         mavenLocal()
@@ -269,7 +259,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath(Libs.PROGUARD)
+        classpath(Dependencies.PROGUARD)
     }
 }
 
