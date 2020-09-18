aa2153117c7e579aa94dc1f048a0542bd51d901b
Janek Góral <jan.goral@gogoapps.io>
2020-09-18 15:49:21 +0200

    Add @JsonProperty("environment-variables") to AppTestPair

--- test_runner/src/main/kotlin/ftl/args/yml/AppTestPair.kt
+++ test_runner/src/main/kotlin/ftl/args/yml/AppTestPair.kt
@@ -1,7 +1,12 @@
 package ftl.args.yml
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class AppTestPair(
     val app: String?,
     val test: String,
+    @JsonProperty("environment-variables")
     val environmentVariables: Map<String, String> = emptyMap()
 )
