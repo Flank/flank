ec38f0a4b5e23141084e1568670c653a7c726740
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-09-18 18:01:46 +0200

    feat: Added tests for auto update dependencies

--- flank-scripts/src/main/kotlin/flank/scripts/dependencies/update/DependenciesUpdateCommand.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/dependencies/update/DependenciesUpdateCommand.kt
@@ -4,7 +4,6 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
-import flank.scripts.dependencies.updateGradle
 import java.io.File
 
 object DependenciesUpdateCommand : CliktCommand(name = "update", help = "Update dependencies") {
---
+++ flank-scripts/src/test/kotlin/flank/scripts/dependencies/update/DependencyExtensionsTest.kt
@@ -0,0 +1,113 @@
+package flank.scripts.dependencies.update
+
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
+
+class DependencyExtensionsTest {
+
+    @Test
+    fun `should return group with name`() {
+        // given
+        val dependency = Dependency(
+            "group",
+            "1.0",
+            "name"
+        )
+
+        // when
+        val actual = dependency.groupWithName
+
+        // then
+        assertEquals("${dependency.group}:${dependency.name}:", actual)
+    }
+
+    @Test
+    fun `should get version to update if release available`() {
+        val dependency = Dependency(
+            "group",
+            "1.0",
+            "name",
+            AvailableVersion(
+                "1.1", null, null
+            )
+        )
+
+        // when
+        val actual = dependency.versionToUpdate
+
+        // then
+        assertEquals(dependency.availableVersion?.release, actual)
+    }
+
+    @Test
+    fun `should get version to update if milestone available`() {
+        val dependency = Dependency(
+            "group",
+            "1.0",
+            "name",
+            AvailableVersion(null, "1.1", null)
+        )
+
+        // when
+        val actual = dependency.versionToUpdate
+
+        // then
+        assertEquals(dependency.availableVersion?.milestone, actual)
+    }
+
+    @Test
+    fun `should get version to update if integration available`() {
+        val dependency = Dependency(
+            "group",
+            "1.0",
+            "name",
+            AvailableVersion(null, null, "1.1")
+        )
+
+        // when
+        val actual = dependency.versionToUpdate
+
+        // then
+        assertEquals(dependency.availableVersion?.integration, actual)
+    }
+
+    @Test
+    fun `should get current version to update if no update`() {
+        val dependency = Dependency(
+            "group",
+            "1.0",
+            "name",
+            null
+        )
+
+        // when
+        val actual = dependency.versionToUpdate
+
+        // then
+        assertEquals(dependency.version, actual)
+    }
+
+    @Test
+    fun `should properly check if gradle needs update`() {
+        // given
+        val gradleWhichNeedsUpdate = GradleDependency(
+            current = GradleVersion("1.1", "test", false, false),
+            nightly = GradleVersion("1.3", "test", false, false),
+            releaseCandidate = GradleVersion("1.2rc", "test", false, false),
+            running = GradleVersion("1", "test", false, false),
+        )
+        val gradleWhichDoesNotNeedUpdate = GradleDependency(
+            current = GradleVersion("1.1", "test", false, false),
+            nightly = GradleVersion("1.3", "test", false, false),
+            releaseCandidate = GradleVersion("1.2rc", "test", false, false),
+            running = GradleVersion("1.1", "test", false, false),
+        )
+
+        // when - then
+        assertTrue(gradleWhichNeedsUpdate.needsUpdate())
+        assertFalse(gradleWhichDoesNotNeedUpdate.needsUpdate())
+    }
+}
