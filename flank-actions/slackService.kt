ac83e87602455440b48ffba78733a282fab54504
Michael Wright <slooxied@gmail.com>
2020-09-25 22:50:01 +0200

    fix: release notes slack tweaks (#1161)
    * Tweaks for sending via slack correctly
    
    * remove unused code

--- flank-actions/slackService.kt
+++ flank-actions/slackService.kt
@@ -6,7 +6,7 @@ fun sendMessage(args: Array<String>): Int{
     val token = args[Common.ARGS_TOKEN]
     val channel = args[Common.ARGS_CHANNEL]
     val message = args[Common.ARGS_MESSAGE]
-    val cookie = args.copyOfRange(Common.ARGS_COOKIE, args.lastIndex).joinToString(" ")
+    val cookie = args[Common.ARGS_COOKIE]
 
     return try {
         val (req, rep, res) = Fuel.get(Common.URL_SLACK, 
@@ -14,19 +14,9 @@ fun sendMessage(args: Array<String>): Int{
             .header(Headers.COOKIE to cookie)
             .responseString()
 
-        debug("Result: $res")
         Common.EXIT_CODE_SUCCESS
     }
     catch(e: Exception) {
-        error("Error has occured: $e")
         Common.EXIT_CODE_FAILURE
     }
-}
-
-fun debug(message: String){
-    println("::debug::$message")
-}
-
-fun error(message: String) {
-    println("::error::$message")
 }
