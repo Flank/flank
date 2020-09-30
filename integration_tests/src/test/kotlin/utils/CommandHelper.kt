b6f591cef3bc4fea67a5a7a3ce1e20cf663a5cc5
Adam <adam.filipowicz92@gmail.com>
2020-09-30 16:41:13 +0200

    Added non wsl windows workflow file

--- integration_tests/src/test/kotlin/utils/CommandHelper.kt
+++ integration_tests/src/test/kotlin/utils/CommandHelper.kt
@@ -5,18 +5,23 @@ import java.util.concurrent.TimeUnit
 
 fun String.runCommand(workingDir: File): ProcessResult = ProcessBuilder(*split("\\s".toRegex()).toTypedArray())
         .directory(workingDir).run {
-            if(System.getProperty("os.name").startsWith("Windows")) redirectOutput(File("out.txt"))
-                        .redirectError(File("err.txt"))
-                        .start().run {
-                            waitFor(60, TimeUnit.MINUTES)
-                            ProcessResult(exitValue(), File("out.txt").readText() + File("err.txt").readText())
-                        }
-             else redirectOutput(ProcessBuilder.Redirect.PIPE)
-                        .redirectError(ProcessBuilder.Redirect.PIPE)
-                        .start().run {
-                            waitFor(60, TimeUnit.MINUTES)
-                            ProcessResult(exitValue(), inputStream.bufferedReader().readText() + this.errorStream.bufferedReader().readText())
-                        }
-
+            if (System.getProperty("os.name").startsWith("Windows"))
+                redirectOutput(File(outputFileName)).
+                redirectError(File(errorFileName)).
+                start().
+                    run {
+                        waitFor(processTimeout, TimeUnit.MINUTES)
+                        ProcessResult(exitValue(), File(outputFileName).readText() + File(errorFileName).readText())
+                    }
+            else redirectOutput(ProcessBuilder.Redirect.PIPE).
+                redirectError(ProcessBuilder.Redirect.PIPE).
+                start().
+                    run {
+                        waitFor(processTimeout, TimeUnit.MINUTES)
+                        ProcessResult(exitValue(), inputStream.bufferedReader().readText() + this.errorStream.bufferedReader().readText())
+                    }
         }
 
+private const val outputFileName = "out.log"
+private const val errorFileName = "error.log"
+private const val processTimeout = 60L
