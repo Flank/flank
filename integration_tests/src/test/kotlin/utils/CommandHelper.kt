4d817ec5bf36d93de6991c3ea34943b9b1a6abf5
Adam <adam.filipowicz92@gmail.com>
2020-09-30 15:04:35 +0200

    Allow tests to get output from flank on windows

--- integration_tests/src/test/kotlin/utils/CommandHelper.kt
+++ integration_tests/src/test/kotlin/utils/CommandHelper.kt
@@ -4,10 +4,19 @@ import java.io.File
 import java.util.concurrent.TimeUnit
 
 fun String.runCommand(workingDir: File): ProcessResult = ProcessBuilder(*split("\\s".toRegex()).toTypedArray())
-    .directory(workingDir)
-    .redirectOutput(ProcessBuilder.Redirect.PIPE)
-    .redirectError(ProcessBuilder.Redirect.PIPE)
-    .start().run {
-        waitFor(60, TimeUnit.MINUTES)
-        ProcessResult(exitValue(), inputStream.bufferedReader().readText() + this.errorStream.bufferedReader().readText())
-    }
+        .directory(workingDir).run {
+            if(System.getProperty("os.name").startsWith("Windows")) redirectOutput(File("out.txt"))
+                        .redirectError(File("err.txt"))
+                        .start().run {
+                            waitFor(60, TimeUnit.MINUTES)
+                            ProcessResult(exitValue(), File("out.txt").readText() + File("err.txt").readText())
+                        }
+             else redirectOutput(ProcessBuilder.Redirect.PIPE)
+                        .redirectError(ProcessBuilder.Redirect.PIPE)
+                        .start().run {
+                            waitFor(60, TimeUnit.MINUTES)
+                            ProcessResult(exitValue(), inputStream.bufferedReader().readText() + this.errorStream.bufferedReader().readText())
+                        }
+
+        }
+
