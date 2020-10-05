c6bc9fb205151f0b0b33970d5518706e5f8309a6
adamfilipow92 <64852261+adamfilipow92@users.noreply.github.com>
2020-10-05 15:50:54 +0200

    feat: Android integration tests run on non wsl windows (#1175)
    * Allow tests to get output from flank on windows
    
    * Added non wsl windows workflow file
    
    * Update windows-non-wsl-workflow.yml
    
    * Minor fixes for windows build
    
    * Neaten CommandHHelper and add misisng bat file
    
    * Name change
    
    * PR comments 1
    
    * PR comments 2
    
    * Return type removal
    
    * Return type removal #2
    
    Co-authored-by: Michael Wright <slooxied@gmail.com>

--- integration_tests/src/test/kotlin/utils/CommandHelper.kt
+++ integration_tests/src/test/kotlin/utils/CommandHelper.kt
@@ -3,11 +3,30 @@ package utils
 import java.io.File
 import java.util.concurrent.TimeUnit
 
+
 fun String.runCommand(workingDir: File): ProcessResult = ProcessBuilder(*split("\\s".toRegex()).toTypedArray())
-    .directory(workingDir)
-    .redirectOutput(ProcessBuilder.Redirect.PIPE)
-    .redirectError(ProcessBuilder.Redirect.PIPE)
-    .start().run {
-        waitFor(60, TimeUnit.MINUTES)
-        ProcessResult(exitValue(), inputStream.bufferedReader().readText() + this.errorStream.bufferedReader().readText())
-    }
+    .directory(workingDir).redirectOutputForCompatibleOS().startProcess()
+
+
+private fun ProcessBuilder.startProcess() = start().run {
+    waitFor(processTimeout, TimeUnit.MINUTES)
+    ProcessResult(exitValue(), getOsSpecificOutput())
+}
+
+private fun Process.getOsSpecificOutput() = if (isWindows) {
+    File(outputFileName).readText() + File(errorFileName).readText()
+} else {
+    inputStream.bufferedReader().readText() + this.errorStream.bufferedReader().readText()
+}
+
+private fun ProcessBuilder.redirectOutputForCompatibleOS() = if (isWindows) {
+    redirectOutput(File(outputFileName)).redirectError(File(errorFileName))
+} else {
+    redirectOutput(ProcessBuilder.Redirect.PIPE).redirectError(ProcessBuilder.Redirect.PIPE)
+}
+
+private val isWindows = System.getProperty("os.name").startsWith("Windows")
+
+private const val outputFileName = "out.log"
+private const val errorFileName = "error.log"
+private const val processTimeout = 60L
