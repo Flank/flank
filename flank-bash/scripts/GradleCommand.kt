54e3a776dda7bb57362b5199bb2a89009ba86b13
Adam <adam.filipowicz92@gmail.com>
2020-10-07 16:30:06 +0200

    GradleCommand, updateFlank - windows compability

--- flank-bash/scripts/GradleCommand.kt
+++ flank-bash/scripts/GradleCommand.kt
@@ -3,4 +3,7 @@ import java.nio.file.Paths
 fun createGradleCommand(
     workingDir: String,
     options: List<String>
-) = "${Paths.get(workingDir, "gradlew").toString()} ${options.joinToString(" ")}"
+) = if (isWindows) "${Paths.get(workingDir, "gradlew.bat").toString()} ${options.joinToString(" ")}"
+else "${Paths.get(workingDir, "gradlew").toString()} ${options.joinToString(" ")}"
+
+private val isWindows = System.getProperty("os.name").startsWith("Windows")
--- flank-bash/scripts/updateFlank.main.kts
+++ flank-bash/scripts/updateFlank.main.kts
@@ -1,5 +1,3 @@
-#!/usr/bin/env kotlin
-
 @file:Repository("https://dl.bintray.com/jakubriegel/kotlin-shell")
 @file:DependsOn("eu.jrie.jetbrains:kotlin-shell-core:0.2.1")
 @file:DependsOn("org.slf4j:slf4j-simple:1.7.28")
