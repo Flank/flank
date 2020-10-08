3907e38af745204422150b247cc3784a194bab9d
Adam <adam.filipowicz92@gmail.com>
2020-10-08 18:10:37 +0200

    Add build of base android apk and tests with copy to directory

--- flank-bash/scripts/android.ops.main.kts
+++ flank-bash/scripts/android.ops.main.kts
@@ -6,19 +6,46 @@
 @file:CompilerOptions("-Xopt-in=kotlin.RequiresOptIn")
 @file:OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import eu.jrie.jetbrains.kotlinshell.shell.*
-
-fun generateApkAndTests() {
-    baseAppApk()
-}
+import java.nio.file.StandardCopyOption
 
 val buildBaseApk = createGradleCommand(
     workingDir = androidTestProjectsPath,
-    options = listOf("-p", androidTestProjectsPath, "app:clean", "app:assemble")
+    options = listOf("-p", androidTestProjectsPath, "app:assemble")
 )
+
+val buildBaseTestApk = createGradleCommand(
+    workingDir = androidTestProjectsPath,
+    options = listOf("-p", androidTestProjectsPath, "app:assembleAndroidTest")
+)
+
+fun generateApkAndTests() {
+    baseAppApk()
+    baseTesApks()
+}
 
 fun baseAppApk() {
     shell {
         buildBaseApk()
+        val assembleDirectory = Paths.get(androidTestProjectsPath, "app", "build", "outputs", "apk", "singleSuccess", "debug", "app-single-success-debug.apk")
+        val outputDir = Paths.get(flankFixturesTmpPath, "apk", "app-debug.apk")
+        if(!outputDir.parent.toFile().exists()){
+            Files.createDirectories(outputDir.parent)
+        }
+        Files.copy(assembleDirectory, outputDir, StandardCopyOption.REPLACE_EXISTING)
+    }
+}
+
+fun baseTesApks() {
+    shell {
+        buildBaseTestApk()
+        val assembleDirectory = Paths.get(androidTestProjectsPath, "app", "build", "outputs", "apk", "androidTest")
+        File(assembleDirectory.toString()).walk().filter { it.extension == "apk" }.forEach {
+            File("test.log").appendText("Test FILE: ${it.toPath()}")
+            Files.copy(it.toPath(), Paths.get(flankFixturesTmpPath, "apk", it.name))
+        }
     }
 }
