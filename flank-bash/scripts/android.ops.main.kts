8e699d2c6f5e1eac5d5ed56a705c4707821fc3c8
Adam <adam.filipowicz92@gmail.com>
2020-10-09 15:37:04 +0200

    Update android.ops.main.kts

--- flank-bash/scripts/android.ops.main.kts
+++ flank-bash/scripts/android.ops.main.kts
@@ -22,31 +22,26 @@ suspend fun Shell.generateApkAndTests() {
 }
 
 suspend fun Shell.baseAppApk() {
-    val buildBaseApk = createGradleCommand(
+    createGradleCommand(
         workingDir = androidTestProjectsPath,
         options = listOf("-p", androidTestProjectsPath, "app:assemble")
-    )
-    buildBaseApk()
-    val assembleDirectory = Paths.get(androidTestProjectsPath, "app", "build", "outputs", "apk", "singleSuccess", "debug", "app-single-success-debug.apk")
+    )()
     val outputDir = Paths.get(flankFixturesTmpPath, "apk", "app-debug.apk")
-    if (!outputDir.parent.toFile().exists()) {
-        Files.createDirectories(outputDir.parent)
-    }
+    if (!outputDir.parent.toFile().exists()) Files.createDirectories(outputDir.parent)
+    val assembleDirectory = Paths.get(androidTestProjectsPath, "app", "build", "outputs", "apk", "singleSuccess", "debug", "app-single-success-debug.apk")
     Files.copy(assembleDirectory, outputDir, StandardCopyOption.REPLACE_EXISTING)
 
 }
 
 suspend fun Shell.baseTesApks() {
-    val buildBaseTestApk = createGradleCommand(
+    createGradleCommand(
         workingDir = androidTestProjectsPath,
         options = listOf("-p", androidTestProjectsPath, "app:assembleAndroidTest")
-    )
-    buildBaseTestApk()
+    )()
     val assembleDirectory = Paths.get(androidTestProjectsPath, "app", "build", "outputs", "apk", "androidTest")
     File(assembleDirectory.toString()).findApks().forEach {
         Files.copy(it.toPath(), Paths.get(flankFixturesTmpPath, "apk", it.name), StandardCopyOption.REPLACE_EXISTING)
     }
-
 }
 
 suspend fun Shell.duplicatedNamesApks() {
@@ -61,28 +56,23 @@ suspend fun Shell.duplicatedNamesApks() {
     modules.map { Paths.get(androidTestProjectsPath, it, "testModule", "build", "outputs", "apk").toFile() }
         .flatMap { it.findApks().toList() }
         .forEachIndexed { index, file ->
-            file.copyDuplicatedApkToDirectory(Paths.get(outputDir.toString(), modules[index], file.name))
+            file.copyApkToDirectory(Paths.get(outputDir.toString(), modules[index], file.name))
         }
 }
 
-fun File.copyDuplicatedApkToDirectory(output: Path) = toPath().let { sourceFile ->
+fun File.copyApkToDirectory(output: Path) = toPath().let { sourceFile ->
     if (!output.parent.toFile().exists()) Files.createDirectories(output.parent)
     Files.copy(sourceFile, output, StandardCopyOption.REPLACE_EXISTING)
 }
 
 suspend fun Shell.multiModulesApks() {
-    val modulesName = (1..20).map { "testModule$it" }
-    val gradleModules = modulesName.map { ":multi-modules:$it:assembleAndroidTest" }
     createGradleCommand(
         workingDir = androidTestProjectsPath,
-        options = listOf("-p", androidTestProjectsPath, ":multi-modules:multiapp:assemble") + gradleModules
+        options = listOf("-p", androidTestProjectsPath, ":multi-modules:multiapp:assemble") + (1..20).map { ":multi-modules:testModule$it:assembleAndroidTest" }
     )()
     val outputDir = Paths.get(flankFixturesTmpPath, "apk", "multi-modules").toString()
     Paths.get(androidTestProjectsPath, "multi-modules").toFile().findApks()
-        .forEach {
-            it.copyDuplicatedApkToDirectory(Paths.get(outputDir, it.name))
-        }
-
+        .forEach { it.copyApkToDirectory(Paths.get(outputDir, it.name)) }
 }
 
 suspend fun Shell.cucumberSampleApp() {
@@ -91,12 +81,11 @@ suspend fun Shell.cucumberSampleApp() {
         options = listOf("-p", androidTestProjectsPath, "cucumber_sample_app:cukeulator:assembleDebug", ":cucumber_sample_app:cukeulator:assembleAndroidTest")
     )()
     val outputDir = Paths.get(flankFixturesTmpPath, "apk", "cucumber_sample_app").toString()
-    Paths.get(androidTestProjectsPath, "cucumber_sample_app").toFile().findApks()
-        .copyApksToPath(outputDir)
+    Paths.get(androidTestProjectsPath, "cucumber_sample_app").toFile().findApks().copyApksToPath(outputDir)
 }
 
 fun File.findApks() = walk().filter { it.extension == "apk" }
 
 fun Sequence<File>.copyApksToPath(outputDirectory: String) = forEach {
-    it.copyDuplicatedApkToDirectory(Paths.get(outputDirectory, it.name))
+    it.copyApkToDirectory(Paths.get(outputDirectory, it.name))
 }
