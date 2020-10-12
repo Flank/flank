638812dd6d3957b199b8e3c8ae2462ab91ae858c
Janek Góral <jan.goral@gogoapps.io>
2020-10-12 13:17:32 +0200

    Fix resolveArtifacts

--- flank-scripts/src/main/kotlin/flank/scripts/testartifacts/core/ResolveArtifacts.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/testartifacts/core/ResolveArtifacts.kt
@@ -1,38 +1,25 @@
 package flank.scripts.testartifacts.core
 
 import com.jcabi.github.Repo
-import java.io.File
 
-fun Context.resolveArtifacts(repo: Repo = testArtifactsRepo()) {
-    resolveRemotelyOrFallback(
-        repo = repo,
-        fallback = { resolveFallback(repo) }
-    )
-}
-
-private fun Context.resolveRemotelyOrFallback(repo: Repo, fallback: () -> Unit) {
+fun Context.resolveArtifacts(
+    repo: Repo = testArtifactsRepo()
+) {
     if (isNewVersionAvailable(repo)) {
         downloadFixtures()
         unzipTestArtifacts()
         linkArtifacts()
-    } else fallback()
-}
-
-private fun Context.resolveFallback(repo: Repo) {
-    projectRoot.testArtifacts(branch).run {
+    } else projectRoot.testArtifacts(branch).run {
         if (exists()) println("* Resolved test artifacts for branch $branch under $absolutePath")
-        else tryResolveForMaster(repo)
+        else copy(branch = "master").run {
+            if (isNewVersionAvailable(repo)) {
+                downloadFixtures()
+                unzipTestArtifacts()
+                linkArtifacts()
+            } else projectRoot.testArtifacts(branch).run {
+                if (exists()) println("* Resolved test artifacts for branch $branch under $absolutePath")
+                else throw Exception("Cannot resolve test artifacts")
+            }
+        }
     }
-}
-
-private fun Context.tryResolveForMaster(repo: Repo) {
-    copy(branch = "master").resolveRemotelyOrFallback(
-        repo = repo,
-        fallback = { projectRoot.testArtifacts(branch).resolveLocallyOrThrow(branch) }
-    )
-}
-
-private fun File.resolveLocallyOrThrow(branch: String) {
-    if (exists()) println("* Resolved test artifacts for branch $branch under $absolutePath")
-    else throw Exception("Cannot resolve test artifacts")
 }
