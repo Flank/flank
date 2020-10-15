4f32ae53da24a68e7f74059d2cb580f56d3445af
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-10-15 14:59:28 +0200

    added scripts for go

---
+++ test_projects/gohello/build.bat
@@ -0,0 +1,1 @@
+ kotlin ..\..\flank-bash\scripts\go.ops.main.kts
--- test_projects/gohello/build.sh
+++ test_projects/gohello/build.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-rm -rf "./bin/"
-mkdir -p bin/win
-mkdir -p bin/linux
-mkdir -p bin/mac
-
-GOOS=windows GOARCH=amd64 go build -o ./bin/win/gohello.exe
-GOOS=linux   GOARCH=amd64 go build -o ./bin/linux/gohello
-GOOS=darwin  GOARCH=amd64 go build -o ./bin/mac/gohello
+kotlin ../../flank-bash/scripts/go.ops.main.kts
