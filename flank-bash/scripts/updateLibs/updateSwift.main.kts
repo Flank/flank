f268981fde9158a2da1de4e297b408634d564770
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-10-15 17:22:19 +0200

    clean up

--- flank-bash/scripts/updateLibs/updateSwift.main.kts
+++ flank-bash/scripts/updateLibs/updateSwift.main.kts
@@ -6,7 +6,6 @@
 
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
 
 private val currentPath = Paths.get("")
 private val swiftPath = Paths.get(currentPath.toString(), "swift")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ include(
     ":test_runner",
     ":firebase_apis:test_api",
     ":flank-scripts",
-    ":flank-bash",
     ":integration_tests",
     "samples:gradle-export-api",
     "test_projects:android"
