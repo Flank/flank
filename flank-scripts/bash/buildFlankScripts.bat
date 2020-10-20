ba350d1ed97db7f6806ded7c21ef2130cdac281c
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-10-20 18:31:46 +0200

    fixed scripts for building flankScripts

--- flank-scripts/bash/buildFlankScripts.bat
+++ flank-scripts/bash/buildFlankScripts.bat
@@ -1,2 +1,7 @@
 SET DIR=%~dp0
-kotlin %DIR%\..\..\flank-bash\scripts\buildFlankScripts.main.kts
+
+SET FLANK_SCRIPTS=%DIR%\..
+SET GRADLE_EXECUTABLE_PATH=%FLANK_SCRIPTS%\..
+
+%GRADLE_EXECUTABLE_PATH%\gradlew.bat flank-scripts:clean flank-scripts:assemble flank-scripts:shadowJar
+copy %FLANK_SCRIPTS%\build\libs\flankScripts.jar %DIR%\flankScripts.jar
--- flank-scripts/bash/buildFlankScripts.sh
+++ flank-scripts/bash/buildFlankScripts.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
 
 DIR=`dirname "$BASH_SOURCE"`
-kotlin $DIR/../../flank-bash/scripts/buildFlankScripts.main.kts
+
+FLANK_SCRIPTS="$DIR/.."
+
+"$FLANK_SCRIPTS/../gradlew" :flank-scripts:clean :flank-scripts:assemble :flank-scripts:shadowJar
+cp "$FLANK_SCRIPTS"/build/libs/flankScripts.jar "$DIR/flankScripts.jar"
