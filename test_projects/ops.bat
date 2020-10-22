c065c0b77484c973f7141fa6dbc88e3e821ce000
Adam <adam.filipowicz92@gmail.com>
2020-10-22 17:03:08 +0200

    Fix build on windows

--- test_projects/ops.bat
+++ test_projects/ops.bat
@@ -1,12 +1,18 @@
+@ECHO OFF
 SET ARG=%1
+if "%ARG%" == "android" (
+	CALL ../flank-scripts/bash/flankScripts.bat shell ops android --copy --generate
+)
 
-if %ARG%=="android" CALL $DIR/../flank-scripts/bash/flankScripts.bat shell ops android --copy --generate
+if "%ARG%" == "ios" (
+    echo iOS Build on windows not supported
+)
 
-if %ARG%=="ios" echo "iOS Build on windows not supported
+if "%ARG%" == "go" (
+	CALL ../flank-scripts/bash/flankScripts.bat shell ops go
+)
 
-if %ARG%=="go" CALL $DIR/../flank-scripts/bash/flankScripts.bat shell ops go --copy --generate
-
-if %ARG%=="all" (
-    CALL $DIR/../flank-scripts/bash/flankScripts.bat shell ops android --copy --generate
-    CALL $DIR/../flank-scripts/bash/flankScripts.bat shell ops go
+if "%ARG%" == "all" (
+    CALL ../flank-scripts/bash/flankScripts.bat shell ops android --copy --generate
+    CALL ../flank-scripts/bash/flankScripts.bat shell ops go
 )
