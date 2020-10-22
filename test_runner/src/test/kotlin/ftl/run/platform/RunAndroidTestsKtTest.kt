88175523d9702b333fa3a11ddd5a20adbb124c3d
Adam <adam.filipowicz92@gmail.com>
2020-10-22 15:06:47 +0200

    Added tests

--- test_runner/src/test/kotlin/ftl/run/platform/RunAndroidTestsKtTest.kt
+++ test_runner/src/test/kotlin/ftl/run/platform/RunAndroidTestsKtTest.kt
@@ -1,10 +1,14 @@
 package ftl.run.platform
 
 import ftl.args.AndroidArgs
+import ftl.gc.GcAndroidTestMatrix
 import ftl.run.model.TestResult
 import ftl.test.util.FlankTestRunner
 import ftl.test.util.mixedConfigYaml
 import ftl.test.util.should
+import io.mockk.every
+import io.mockk.mockkObject
+import io.mockk.verify
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -33,5 +37,55 @@ class RunAndroidTestsKtTest {
 
         // then
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should add additional index if repeatTests set`() {
+        val androidArgs = AndroidArgs.load(mixedConfigYaml)
+        mockkObject(GcAndroidTestMatrix, androidArgs) {
+
+            every { androidArgs.resultsDir } returns "test_dir"
+            every { androidArgs.resultsBucket } returns "test_bucket"
+            every { androidArgs.repeatTests } returns 2
+
+            runBlocking {
+                runAndroidTests(androidArgs)
+            }
+
+            verify {
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_0/", any(), any(), any(), any())
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_0_1/", any(), any(), any(), any())
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_1/", any(), any(), any(), any())
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_1_1/", any(), any(), any(), any())
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_2/", any(), any(), any(), any())
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_2_1/", any(), any(), any(), any())
+            }
+
+            verify(inverse = true) {
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_0_2/", any(), any(), any(), any())
+            }
+        }
+    }
+
+    @Test
+    fun `shouldn't add additional index if repeatTests not set`() {
+        val androidArgs = AndroidArgs.load(mixedConfigYaml)
+        mockkObject(GcAndroidTestMatrix, androidArgs) {
+
+            every { androidArgs.resultsDir } returns "test_dir"
+            every { androidArgs.resultsBucket } returns "test_bucket"
+
+            runBlocking {
+                runAndroidTests(androidArgs)
+            }
+
+            verify(inverse = true) {
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_0_1/", any(), any(), any(), any())
+            }
+
+            verify {
+                GcAndroidTestMatrix.build(any(), any(), "test_dir/matrix_0/", any(), any(), any(), any())
+            }
+        }
     }
 }
