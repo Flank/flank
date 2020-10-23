a52cc3dcaddd73f532299f910ceac3020ff798a7
Jan Góral <60390247+jan-gogo@users.noreply.github.com>
2020-10-23 10:20:54 +0200

    Update test_runner/src/main/kotlin/ftl/ios/Xctestrun.kt

--- test_runner/src/main/kotlin/ftl/ios/Xctestrun.kt
+++ test_runner/src/main/kotlin/ftl/ios/Xctestrun.kt
@@ -10,12 +10,6 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.nio.file.Paths
 
-/*
-{
-  "TestTarget": ["method1", "method2"],
-  "TestTarget2": ["method3", "method4"]
-}
-*/
 typealias XctestrunMethods = Map<String, List<String>>
 
 object Xctestrun {
