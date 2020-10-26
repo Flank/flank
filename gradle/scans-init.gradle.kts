12cdcb1c35cc95ac17ffe27c4afee6dc84c65924
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-10-26 09:36:09 +0100

    Add build scann for ubuntu workflow

---
+++ gradle/scans-init.gradle.kts
@@ -0,0 +1,48 @@
+import com.gradle.enterprise.gradleplugin.GradleEnterprisePlugin
+import com.gradle.scan.plugin.BuildScanPlugin
+import java.io.ByteArrayOutputStream
+
+/**
+ * We want to publish scans only for top level build and when explicitly selected.
+ * To publish scan within the build, run
+ * ./gradlew build -I gradle/scans-init.gradle.kts
+ */
+initscript {
+    val pluginVersion = "3.4.1"
+
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.gradle:gradle-enterprise-gradle-plugin:${pluginVersion}")
+    }
+}
+
+val isTopLevelBuild = gradle.parent == null
+
+if (isTopLevelBuild) {
+    beforeSettings {
+        if (!pluginManager.hasPlugin("com.gradle.enterprise")) {
+            pluginManager.apply(GradleEnterprisePlugin::class)
+        }
+        configureExtension(extensions["gradleEnterprise"])
+    }
+}
+
+fun configureExtension(extension: Any) {
+    extension.withGroovyBuilder {
+        getProperty("buildScan").withGroovyBuilder {
+            setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+            setProperty("termsOfServiceAgree", "yes")
+            "publishAlways"()
+            "background"() {
+                val os = ByteArrayOutputStream()
+                exec {
+                    commandLine("git", "rev-parse", "--verify", "HEAD")
+                    standardOutput = os
+                }
+                "value"("Git Commit ID", os.toString())
+            }
+        }
+    }
+}
