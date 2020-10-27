acda8a7e9d415b349e48ddbd0db7bb94d6164523
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-10-27 15:09:54 +0100

    Move to settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream
+
 rootProject.name = "flank"
 
 include(
@@ -8,3 +10,22 @@ include(
     "samples:gradle-export-api",
     "test_projects:android"
 )
+
+plugins {
+    id("com.gradle.enterprise") version "3.4.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        background {
+            val os = ByteArrayOutputStream()
+            exec {
+                commandLine("git", "rev-parse", "--verify", "HEAD")
+                standardOutput = os
+            }
+            value("Git Commit ID", os.toString())
+        }
+    }
+}
