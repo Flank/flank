4f7888f9960b0c982f7c604cfaf2da6aa415a04b
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-11-12 16:27:24 +0100

    add some tests and refactor mock server

--- flank-scripts/src/main/kotlin/flank/scripts/pullrequest/SetLabels.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/pullrequest/SetLabels.kt
@@ -3,30 +3,29 @@ package flank.scripts.pullrequest
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitResult
 import com.github.kittinunf.fuel.coroutines.awaitStringResult
+import com.github.kittinunf.result.getOrNull
 import com.github.kittinunf.result.map
 import com.github.kittinunf.result.onError
 import com.github.kittinunf.result.success
 import flank.scripts.exceptions.mapClientError
 import flank.scripts.exceptions.toGithubException
-import flank.scripts.github.GithubPullRequestDeserializer
+import flank.scripts.github.GitHubLabelDeserializable
 import flank.scripts.utils.toJson
 import kotlinx.serialization.Serializable
 
 suspend fun copyLabels(githubToken: String, issueNumber: Int, pullRequestNumber: Int) {
     getLabelsFromIssue(githubToken, issueNumber)
         .onError { println("Could not copy labels because of ${it.message}") }
-        .map { it.labels.map { label -> label.name } }
-        .get()
-        .run {
-            setLabelsToPullRequest(githubToken, pullRequestNumber, this)
-        }
+        .map { it.map { label -> label.name } }
+        .getOrNull()
+        ?.run { setLabelsToPullRequest(githubToken, pullRequestNumber, this) }
 }
 
 private suspend fun getLabelsFromIssue(githubToken: String, issueNumber: Int) =
     Fuel.get("https://api.github.com/repos/Flank/flank/issues/$issueNumber/labels")
         .appendHeader("Accept", "application/vnd.github.v3+json")
         .appendHeader("Authorization", "token $githubToken")
-        .awaitResult(GithubPullRequestDeserializer)
+        .awaitResult(GitHubLabelDeserializable)
         .mapClientError { it.toGithubException() }
 
 private suspend fun setLabelsToPullRequest(githubToken: String, pullRequestNumber: Int, labels: List<String>) {
---
+++ flank-scripts/src/test/kotlin/flank/scripts/FuelMockServer.kt
@@ -0,0 +1,43 @@
+package flank.scripts
+
+import com.github.kittinunf.fuel.core.Client
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.requests.DefaultBody
+import flank.scripts.release.updatebugsnag.BugSnagRequest
+import flank.scripts.release.updatebugsnag.BugSnagResponse
+import flank.scripts.utils.toJson
+import flank.scripts.utils.toObject
+
+class FuelMockServer : Client {
+    override fun executeRequest(request: Request): Response {
+        val url = request.url.toString()
+        return when {
+            url.startsWith("https://api.github.com/repos/flank/flank/", ignoreCase = true) -> handleGithubMockRequest(url, request)
+            url == "https://build.bugsnag.com/" -> request.handleBugsnagResponse()
+            else -> Response(request.url)
+        }
+    }
+}
+
+private fun Request.handleBugsnagResponse() =
+    if (body.asString("application/json").toObject<BugSnagRequest>().apiKey == "success") {
+        buildResponse(
+            body = BugSnagResponse("success").toJson(),
+            statusCode = 200
+        )
+    } else {
+        buildResponse(
+            body = BugSnagResponse(
+                status = "failure",
+                errors = listOf("errors")
+            ).toJson(),
+            statusCode = 422
+        )
+    }
+
+fun Request.buildResponse(body: String, statusCode: Int) =
+    Response(url, statusCode = statusCode, responseMessage = body, body = DefaultBody(
+        { body.byteInputStream() },
+        { body.length.toLong() }
+    ))
