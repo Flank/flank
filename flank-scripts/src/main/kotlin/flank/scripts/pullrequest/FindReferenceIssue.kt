34d0d0bde5229bb65a57c8701c250b0df42e4166
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2020-11-12 14:05:12 +0100

    Clean up code

--- flank-scripts/src/main/kotlin/flank/scripts/pullrequest/FindReferenceIssue.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/pullrequest/FindReferenceIssue.kt
@@ -1,14 +1,16 @@
 package flank.scripts.pullrequest
 
-fun GitHubPullRequest.findReferenceNumber() =
+import flank.scripts.github.GithubPullRequest
+
+fun GithubPullRequest.findReferenceNumber() =
     (tryGetReferenceNumberFromBody() ?: tryGetReferenceNumberFromBranch())
         ?.trim()
         ?.replace("#", "")
         ?.toInt()
 
-private fun GitHubPullRequest.tryGetReferenceNumberFromBody() = bodyReferenceRegex.find(body)?.value
+private fun GithubPullRequest.tryGetReferenceNumberFromBody() = bodyReferenceRegex.find(body)?.value
 
-private fun GitHubPullRequest.tryGetReferenceNumberFromBranch() = branchReferenceRegex.find(head?.ref.orEmpty())?.value
+private fun GithubPullRequest.tryGetReferenceNumberFromBranch() = branchReferenceRegex.find(head?.ref.orEmpty())?.value
 
 private val bodyReferenceRegex = "#\\d+\\s".toRegex()
 private val branchReferenceRegex = "#\\d+".toRegex()
