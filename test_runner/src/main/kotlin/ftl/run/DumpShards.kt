2bb33eb2fd65ba18d39d3423c51fd8d30685d7a0
Jan Góral <60390247+jan-gogo@users.noreply.github.com>
2020-11-23 20:50:25 +0100

    Update test_runner/src/main/kotlin/ftl/run/DumpShards.kt

--- test_runner/src/main/kotlin/ftl/run/DumpShards.kt
+++ test_runner/src/main/kotlin/ftl/run/DumpShards.kt
@@ -14,7 +14,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 suspend fun AndroidArgs.dumpShards(
-    @VisibleForTesting
+    // VisibleForTesting
     shardFilePath: String = ANDROID_SHARD_FILE,
 ) {
     if (!isInstrumentationTest) throw FlankConfigurationError(
