46278809bb9046a98b04152a97015fe68739725a
Axel Zuziak <axel.zuziak@gogoapps.io>
2020-11-26 15:35:44 +0100

    Applied code reviews comments

--- flank-scripts/src/main/kotlin/flank/scripts/shell/ops/iOSOpsCommand.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/shell/ops/iOSOpsCommand.kt
@@ -1,24 +1,15 @@
 package flank.scripts.shell.ops
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.option
 
 object IosOpsCommand : CliktCommand(
-    name = "build_all_ios", 
-    help = "Build all ios test artifacts"
+    name = "ios",
+    help = "Build ios test artifacts"
 ) {
 
-    private val generate: Boolean by option(help = "Make build").flag("-g", default = true)
-
-    override fun run() {
-        if (generate.not()) return
-        listOf(
+    override fun run() = listOf(
             BuildEarlGreyExampleCommand,
             BuildTestPlansExample,
             BuildFlankExampleCommand
-        ).forEach {
-            it.run()
-        }
-    }
+        ).forEach(CliktCommand::run)
 }
