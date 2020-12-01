68a4909735a2ecbedcaff735351ad6ee5f63aa38
piotradamczyk5 <65554637+piotradamczyk5@users.noreply.github.com>
2020-12-01 23:03:21 +0100

    fix: Add fallback when cannot get reference to avoid failing tests on release (#1369)
    ## Test Plan
    > How do we know the code works?
    
    When cannot get the branch name from git or `HEAD_REF` is missing. Tests artifacts are downloaded for master branch

--- flank-scripts/src/main/kotlin/flank/scripts/utils/Git.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/utils/Git.kt
@@ -3,4 +3,10 @@ package flank.scripts.utils
 fun currentGitBranch(): String = "git branch --show-current".runForOutput()
     .trim()
     .takeIf(String::isNotBlank)
-    ?: getEnv("HEAD_REF")
+    ?: getFromEnvironmentVariableOrFallbackToMaster()
+
+private fun getFromEnvironmentVariableOrFallbackToMaster() =
+    runCatching { getEnv("HEAD_REF") }
+        .getOrDefault("")
+        .takeIf(String::isNotBlank)
+        ?: "master"
