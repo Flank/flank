337a32c6041bd347b96daa8e6e408794797268f4
Axel Zuziak <axel.zuziak@gogoapps.io>
2020-12-01 11:13:58 +0100

    Fixed typos in building flank scripts

--- flank-scripts/bash/buildFlankScripts.sh
+++ flank-scripts/bash/buildFlankScripts.sh
@@ -5,4 +5,4 @@ DIR=`dirname "$BASH_SOURCE"`
 FLANK_SCRIPTS="$DIR/.."
 
 "$FLANK_SCRIPTS/../gradlew" :flank-scripts:clean :flank-scripts:assemble :flank-scripts:shadowJar
-cp "$FLANK_SCRIPTS"/build/libs/flankScripts.jar "$DIR/flankScripts.jar"
+cp "$FLANK_SCRIPTS"/build/libs/flank-scripts.jar "$DIR/flank-scripts.jar"
