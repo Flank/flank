e1586f8b83df774ee6a611843c5c27c393837063
Adam <adam.filipowicz92@gmail.com>
2021-02-03 15:51:01 +0100

    added enable-output-report to printing

--- test_runner/src/main/kotlin/ftl/args/AndroidArgs.kt
+++ test_runner/src/main/kotlin/ftl/args/AndroidArgs.kt
@@ -119,6 +119,7 @@ AndroidArgs
       disable-results-upload: $disableResultsUpload
       default-class-test-time: $defaultClassTestTime
       disable-usage-statistics: $disableUsageStatistics
+      enable-output-report: $enableOutputReport
         """.trimIndent()
     }
 }
