0d65a7082212f5ad2b77bc9cc90bb68c8db2ab16
Adam <adam.filipowicz92@gmail.com>
2021-02-18 00:30:01 +0100

    Added IT for *_dump.json

--- integration_tests/src/test/kotlin/integration/DumpShardsIT.kt
+++ integration_tests/src/test/kotlin/integration/DumpShardsIT.kt
@@ -6,6 +6,10 @@ import flank.common.isWindows
 import org.junit.Assume
 import org.junit.Test
 import run
+import utils.containsAll
+import utils.loadAndroidDumpShards
+import utils.loadIosDumpShards
+import java.io.File
 
 class DumpShardsIT {
     private val name = this::class.java.simpleName
@@ -27,6 +31,28 @@ class DumpShardsIT {
         val resOutput = result.output.removeUnicode()
         Truth.assertThat(resOutput).containsMatch(findInCompare(name))
         assertNoOutcomeSummary(resOutput)
+
+        val matrix = File("android_shards.json").loadAndroidDumpShards()
+
+        Truth.assertThat(matrix.shards.count()).isEqualTo(2)
+
+        Truth.assertThat(matrix.shards.values.flatten()).containsAll(
+            "class com.example.test_app.parametrized.EspressoParametrizedClassParameterizedNamed",
+            "class com.example.test_app.parametrized.EspressoParametrizedClassTestParameterized",
+            "class com.example.test_app.ParameterizedTest",
+            "class com.example.test_app.parametrized.EspressoParametrizedMethodTestJUnitParamsRunner",
+        )
+
+        Truth.assertThat(matrix.junitIgnored.count()).isEqualTo(4)
+        Truth.assertThat(matrix.junitIgnored).containsNoDuplicates()
+
+        Truth.assertThat(matrix.junitIgnored)
+            .containsExactly(
+                "class com.example.test_app.InstrumentedTest#ignoredTestWitSuppress",
+                "class com.example.test_app.InstrumentedTest#ignoredTestWithIgnore",
+                "class com.example.test_app.bar.BarInstrumentedTest#ignoredTestBar",
+                "class com.example.test_app.foo.FooInstrumentedTest#ignoredTestFoo"
+            )
     }
 
     @Test
@@ -47,5 +73,15 @@ class DumpShardsIT {
         val resOutput = result.output.removeUnicode()
         Truth.assertThat(resOutput).containsMatch(findInCompare(name))
         assertNoOutcomeSummary(resOutput)
+
+        val shards = File("ios_shards.json").loadIosDumpShards()
+
+        Truth.assertThat(shards.count()).isEqualTo(2)
+
+        shards.first().let { firstShard ->
+            Truth.assertThat(firstShard.count()).isEqualTo(8)
+            Truth.assertThat(firstShard)
+                .contains("EarlGreyExampleSwiftTests/testWithCustomFailureHandler")
+        }
     }
 }
