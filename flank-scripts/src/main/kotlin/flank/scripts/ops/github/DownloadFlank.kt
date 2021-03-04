b499346df5013ee9a9dc652953f23851bc254b56
Adam <adam.filipowicz92@gmail.com>
2021-03-04 18:20:03 +0100

    Added tests for download_flank command

--- flank-scripts/src/main/kotlin/flank/scripts/ops/github/DownloadFlank.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/ops/github/DownloadFlank.kt
@@ -1,14 +1,16 @@
 package flank.scripts.ops.github
 
 import com.github.kittinunf.result.map
+import com.google.common.annotations.VisibleForTesting
 import flank.common.downloadFile
 import flank.scripts.data.github.getLatestReleaseTag
 
 suspend fun downloadFlank(version: String?) =
     version.getVersion().prepareDownloadUrl().downloadFlank()
 
-private suspend fun String?.getVersion(): String =
-    this ?: getLatestReleaseTag("").map { it.tag }.get()
+@VisibleForTesting
+internal suspend fun String?.getVersion(): String =
+    takeUnless { it.isNullOrBlank() } ?: getLatestReleaseTag("").map { it.tag }.get()
 
 private fun String.prepareDownloadUrl() = "https://github.com/Flank/flank/releases/download/$this/$FLANK_JAR"
 
---
+++ flank-scripts/src/test/kotlin/flank/scripts/ops/github/DownloadFlankTest.kt
@@ -0,0 +1,40 @@
+package flank.scripts.ops.github
+
+import com.github.kittinunf.result.map
+import flank.scripts.data.github.getLatestReleaseTag
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+
+class DownloadFlankTest {
+
+    @Test
+    fun `Should return latest tag if version is null`() {
+        val downloadedVersion = runBlocking { null.getVersion() }
+        val latestVersion = runBlocking { getLatestReleaseTag("").map { it.tag }.get() }
+        Assert.assertEquals(downloadedVersion, latestVersion)
+    }
+
+    @Test
+    fun `Should return latest tag if version is blank`() {
+        val downloadedVersion = runBlocking { " ".getVersion() }
+        val latestVersion = runBlocking { getLatestReleaseTag("").map { it.tag }.get() }
+        Assert.assertEquals(downloadedVersion, latestVersion)
+    }
+
+    @Test
+    fun `Should return latest tag if version is empty`() {
+        val downloadedVersion = runBlocking { "".getVersion() }
+        val latestVersion = runBlocking { getLatestReleaseTag("").map { it.tag }.get() }
+        Assert.assertEquals(downloadedVersion, latestVersion)
+    }
+
+    @Test
+    fun `Should return specified release tag`() {
+        val expectedReleaseTag = "v.1.1.1"
+
+        val downloadedVersion = runBlocking { expectedReleaseTag.getVersion() }
+
+        Assert.assertEquals(downloadedVersion, expectedReleaseTag)
+    }
+}
