7c6f62b3cee606c4e28b85bbd8e60363abb51d1e
adamfilipow92 <64852261+adamfilipow92@users.noreply.github.com>
2021-03-09 15:33:42 +0100

    fix: Saving service account by flank script (#1671)
    Fixes #
    
    ## Test Plan
    > How do we know the code works?
    
    When ```./config/gcloud``` path does not exist flank scripts should create directories.

--- flank-scripts/src/main/kotlin/flank/scripts/ops/firebase/SaveServiceAccount.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/ops/firebase/SaveServiceAccount.kt
@@ -8,10 +8,13 @@ import java.nio.file.Path
 fun saveServiceAccount(
     serviceAccount: String,
     serviceAccountPath: Path = defaultCredentialPath.toAbsolutePath()
-) = when {
-    serviceAccount.startsWith("http", true) -> downloadFile(serviceAccount, serviceAccountPath)
-    serviceAccount.endsWith(".json", true) -> saveFromFile(serviceAccount, serviceAccountPath)
-    else -> saveFromStr(serviceAccount, serviceAccountPath)
+) {
+    serviceAccountPath.parent.toFile().mkdirs()
+    when {
+        serviceAccount.startsWith("http", true) -> downloadFile(serviceAccount, serviceAccountPath)
+        serviceAccount.endsWith(".json", true) -> saveFromFile(serviceAccount, serviceAccountPath)
+        else -> saveFromStr(serviceAccount, serviceAccountPath)
+    }
 }
 
 private fun saveFromFile(
