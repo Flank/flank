0f96d9becf6bc9ec8b36f6918805afcc4a4426a0
piotradamczyk5 <65554637+piotradamczyk5@users.noreply.github.com>
2021-03-24 09:37:16 +0100

    test: Added app with many tests (#1730)
    Relates to #1650
    
    ## Test Plan
    > How do we know the code works?
    
    Tests app with many tests could be built.
    
    It is needed for example to add IT in #1650

--- flank-scripts/build.gradle.kts
+++ flank-scripts/build.gradle.kts
@@ -26,7 +26,7 @@ shadowJar.apply {
     }
 }
 // <breaking change>.<feature added>.<fix/minor change>
-version = "1.9.2"
+version = "1.9.3"
 group = "com.github.flank"
 
 application {
---
+++ test_projects/android/app_many_tests/src/androidTest/java/com/example/app/many/tests/ManyTests.kt
@@ -0,0 +1,308 @@
+package com.example.app.many.tests
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ManyTests {
+
+    @Test
+    fun test0() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test1() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test3() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test4() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test5() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test6() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test7() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test8() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test9() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test10() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test11() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test12() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test13() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test14() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test15() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test16() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test17() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test18() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test19() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test20() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test21() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test22() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test23() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test24() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test25() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test26() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test27() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test28() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test29() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test30() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test31() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test32() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test33() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test34() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test35() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test36() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test37() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test38() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test39() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test40() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test41() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test42() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test43() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test44() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test45() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test46() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test47() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test48() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test49() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test50() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test51() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test52() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test53() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test54() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test55() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test56() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test57() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test58() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+    @Test
+    fun test59() {
+        assertEquals(true, "true".toBoolean())
+    }
+
+}
