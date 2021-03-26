94ed5800df00ea7b5f5aa414d637eff6310e36b2
Michael Wright <Slooxied@gmail.com>
2021-03-26 10:01:14 +0100

    Fixes version 1

--- test_runner/src/main/kotlin/ftl/util/Bash.kt
+++ test_runner/src/main/kotlin/ftl/util/Bash.kt
@@ -4,18 +4,19 @@ import flank.common.isWindows
 import flank.common.logLn
 import ftl.run.exception.FlankGeneralError
 import java.lang.ProcessBuilder.Redirect.PIPE
-
 object Bash {
 
-    fun execute(cmd: String): String {
+    fun execute(cmd: String, additionalPath: String = ""): String {
         logLn(cmd)
 
         val bashPath = if (isWindows) "bash.exe" else "/bin/bash"
 
-        val process = ProcessBuilder(bashPath, "-c", cmd)
-            .redirectOutput(PIPE)
-            .redirectError(PIPE)
-            .start()
+        val process = ProcessBuilder(bashPath, "-c", cmd).also {
+            if (additionalPath.isNotEmpty()) {
+                val envs: MutableMap<String, String> = it.environment()
+                envs["Path"] = additionalPath
+            }
+        }.redirectOutput(PIPE).redirectError(PIPE).start()
 
         val result = process.waitForResult()
 
