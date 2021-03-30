1fe7c7ec7dadb836b0ef0ad8e938cd26e72f27cb
adamfilipow92 <64852261+adamfilipow92@users.noreply.github.com>
2021-03-30 10:07:39 +0200

    Update flank-scripts/src/main/kotlin/flank/scripts/utils/Env.kt
    Co-authored-by: Jan Góral <60390247+jan-gogo@users.noreply.github.com>

--- flank-scripts/src/main/kotlin/flank/scripts/utils/Env.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/utils/Env.kt
@@ -1,9 +1,5 @@
 package flank.scripts.utils
 
-fun getEnv(key: String): String =
-    System.getenv(key) ?: throw RuntimeException("Environment variable '$key' not found!")
-
-fun getEnvOrDefault(key: String, default: String): String =
-    System.getenv(key) ?: default
-
+fun getEnv(key: String, default: String? = null): String =
+    System.getenv(key) ?: default ?: throw RuntimeException("Environment variable '$key' not found!")
 val isWindows: Boolean = System.getProperty("os.name").startsWith("Windows")
