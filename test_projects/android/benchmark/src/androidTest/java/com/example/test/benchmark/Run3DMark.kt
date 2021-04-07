8eec0029ef9c9920b2c5112c6c784eebae5ca5b5
Janek Góral <jan.goral@gogoapps.io>
2021-04-07 11:45:06 +0200

    Update

--- test_projects/android/benchmark/src/androidTest/java/com/example/test/benchmark/Run3DMark.kt
+++ test_projects/android/benchmark/src/androidTest/java/com/example/test/benchmark/Run3DMark.kt
@@ -1,4 +1,4 @@
-package com.example.benchmarkroboscript
+package com.example.test.benchmark
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
