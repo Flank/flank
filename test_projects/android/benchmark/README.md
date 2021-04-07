82c2a6ca85bf98e7c9023dd800d961d60fb5755d
Janek Góral <jan.goral@gogoapps.io>
2021-04-07 09:39:57 +0200

    Add 3dmark instrumented test

---
+++ test_projects/android/benchmark/README.md
@@ -0,0 +1,14 @@
+# Automated benchmark
+
+Instrumented test based on ui-automator responsible for running benchmark on tested device
+
+## Benchmarks
+
+* `3dmarkandroid-v2-1-4726.apk`
+* `Geekbench 5_v5.3.2.apk` - TODO
+
+## Devices
+
+* Pixel 5e (physical) - API 30
+* NexusLowRes (virtual) - API 30
+* NexusLowResEmulator (emulator) - API 30
---
+++ test_projects/android/benchmark/src/androidTest/java/com/example/benchmarkroboscript/Run3DMark.kt
@@ -0,0 +1,104 @@
+package com.example.benchmarkroboscript
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.Until
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+private const val TIMEOUT = 10_000L
+
+private const val BENCHMARK_TIMEOUT = 15 * 60 * 1000L
+
+private const val DOWNLOAD_TIMEOUT = 5 * 60 * 1000L
+
+private object Text {
+    const val search = "Search"
+    const val ok = "OK"
+    const val allow = "Allow"
+    const val permTitle = "Before we start…"
+    const val appName = "3DMark"
+    const val benchmarkType = "SLING SHOT"
+}
+
+private object Res {
+    const val btnSkip = "com.futuremark.dmandroid.application:id/flm_bt_tutorial_skip"
+    const val fabBenchmark = "com.futuremark.dmandroid.application:id/flm_fab_benchmark"
+    const val fabSettings = "com.futuremark.dmandroid.application:id/flm_fab_settings"
+    const val scoreDetails = "com.futuremark.dmandroid.application:id/flm_ll_score_details_container"
+}
+
+
+@RunWith(AndroidJUnit4::class)
+class Run3DMark {
+    @Test
+    fun run() {
+
+        UiDevice.getInstance(
+            InstrumentationRegistry.getInstrumentation()
+        ).run {
+            // Start from the home screen
+            pressHome()
+
+            // Open apps menu on pixel launcher
+            findObject(UiSelector().descriptionContains(Text.search)).apply {
+                swipe(0, visibleBounds.centerY(), 0, 0, 10)
+            }
+
+            // Wait for 3d mark launcher icon
+            wait(Until.hasObject(By.text(Text.appName)), TIMEOUT)
+
+            // Click 3d mark launcher icon
+            findObject(UiSelector().text(Text.appName)).click()
+
+            // Check permissions dialog
+            if (findObject(UiSelector().text(Text.permTitle)).exists()) {
+                findObject(UiSelector().text(Text.ok)).click()
+                findObject(UiSelector().text(Text.allow)).click()
+            }
+
+            Thread.sleep(3000)
+
+            // Skip tutorial if needed
+            findObject(UiSelector().resourceId(Res.btnSkip)).apply {
+                if (exists()) click()
+            }
+
+            // Choose proper benchmark screen
+            findObject(UiSelector().text(Text.benchmarkType)).click()
+
+            // Settings fab is not visible if the additional software is not installed
+            if (findObject(UiSelector().resourceId(Res.fabSettings)).exists().not()) {
+
+                // Install additional software
+                findObject(UiSelector().resourceId(Res.fabBenchmark)).click()
+
+                // Wait until download finish
+                wait(
+                    Until.hasObject(By.res(Res.fabSettings)),
+                    DOWNLOAD_TIMEOUT
+                )
+            }
+
+            // Run benchmark
+            findObject(UiSelector().resourceId(Res.fabBenchmark)).click()
+
+
+            Thread.sleep(3000)
+
+            // Wait until benchmark finish
+            wait(Until.hasObject(By.res(Res.scoreDetails)), BENCHMARK_TIMEOUT)
+
+            // Assert that benchmark results screen is visible
+            Assert.assertTrue(findObject(UiSelector().resourceId(Res.scoreDetails)).exists())
+
+            // Make sure that results was recorded
+            Thread.sleep(5000)
+        }
+    }
+}
