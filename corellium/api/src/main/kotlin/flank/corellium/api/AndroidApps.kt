c42154424b7fefe2a7e0c780afd9d6e34476f82a
Jan Góral <60390247+jan-gogo@users.noreply.github.com>
2021-04-26 16:51:41 +0200

    Update corellium/api/src/main/kotlin/flank/corellium/api/AndroidApps.kt
    Co-authored-by: Michael Wright <slooxied@gmail.com>

--- corellium/api/src/main/kotlin/flank/corellium/api/AndroidApps.kt
+++ corellium/api/src/main/kotlin/flank/corellium/api/AndroidApps.kt
@@ -1,7 +1,7 @@
 package flank.corellium.api
 
 /**
- * The [AndroidApps] is representing a bunch of apk files related to the testing instance.
+ * [AndroidApps] represents a bunch of apk files related to the testing instance.
  *
  * @property instanceId the id of the instance where the apps will be installed.
  * @property paths the list of relative paths to the app (apk) files.
