73047eec386a7e6af2b00c57f76fcb7642e14c19
Pawel Pasterz <pawel.pasterz@gmail.com>
2021-04-29 19:45:25 +0200

    Refactor DescribeNetworkProfiles logging

--- test_runner/src/main/kotlin/ftl/domain/DescribeNetworkProfiles.kt
+++ test_runner/src/main/kotlin/ftl/domain/DescribeNetworkProfiles.kt
@@ -1,14 +1,14 @@
 package ftl.domain
 
-import flank.common.logLn
 import ftl.environment.networkProfileDescription
+import ftl.presentation.Output
 import ftl.run.exception.FlankConfigurationError
 
-interface DescribeNetworkProfiles {
+interface DescribeNetworkProfiles : Output {
     val profileId: String
 }
 
 operator fun DescribeNetworkProfiles.invoke() {
     if (profileId.isBlank()) throw FlankConfigurationError("Argument PROFILE_ID must be specified.")
-    logLn(networkProfileDescription(profileId))
+    networkProfileDescription(profileId).out()
 }
--- test_runner/src/main/kotlin/ftl/environment/NetworkProfileDescription.kt
+++ test_runner/src/main/kotlin/ftl/environment/NetworkProfileDescription.kt
@@ -5,47 +5,19 @@ import ftl.api.fetchNetworkProfiles
 
 fun networkProfileDescription(profile: String) = fetchNetworkProfiles()
     .find { it.id.equals(profile, ignoreCase = true) }
-    .toNullProof()
-    .prepareDescription()
     ?: "Unable to fetch profile [$profile] description"
 
-private fun NetworkProfile?.toNullProof() = this?.run {
-    NetworkConfigurationWrapper(
-        downRule = wrappedOrEmpty(downRule),
-        upRule = wrappedOrEmpty(upRule),
-        id = id.toStringOrUnableToFetch()
-    )
-}
-
-private fun wrappedOrEmpty(rule: NetworkProfile.Rule?) = rule?.let {
-    Rule(
-        bandwidth = it.bandwidth.toStringOrUnableToFetch(),
-        delay = it.delay.toStringOrUnableToFetch(),
-        packetLossRatio = it.packetLossRatio.toStringOrUnableToFetch()
-    )
-} ?: emptyRule
-
-private const val UNABLE = "[Unable to fetch]"
-
-private fun Any?.toStringOrUnableToFetch() = this?.toString() ?: UNABLE
-
-private val emptyRule: Rule
-    get() = Rule(UNABLE, UNABLE, UNABLE)
-
-private fun NetworkConfigurationWrapper?.prepareDescription() = this?.run {
+fun NetworkProfile.prepareDescription() =
     """
 downRule:
-  bandwidth: ${downRule.bandwidth}
-  delay: ${downRule.delay}
-  packetLossRatio: ${downRule.packetLossRatio}
+  bandwidth: ${downRule.bandwidth.orUnable()}
+  delay: ${downRule.delay.orUnable()}
+  packetLossRatio: ${downRule.packetLossRatio.orUnable()}
 id: $id
 upRule:
-  bandwidth: ${upRule.bandwidth}
-  delay: ${upRule.delay}
-  packetLossRatio: ${upRule.packetLossRatio}
+  bandwidth: ${upRule.bandwidth.orUnable()}
+  delay: ${upRule.delay.orUnable()}
+  packetLossRatio: ${upRule.packetLossRatio.orUnable()}
     """.trimIndent()
-}
 
-private data class NetworkConfigurationWrapper(val downRule: Rule, val upRule: Rule, val id: String)
-
-private data class Rule(val bandwidth: String, val delay: String, val packetLossRatio: String)
+private fun Any?.orUnable() = this ?: "[Unable to fetch]"
