26d516ef1814df6e9aa04b740ab3910823cfc9aa
piotradamczyk5 <65554637+piotradamczyk5@users.noreply.github.com>
2021-05-04 15:24:49 +0200

    refactor: Structural output Android models describe (#1887)
    Fixes #1846
    
    ## Test Plan
    > How do we know the code works?
    
    - Code is refactored according to #1846
    - Android describe models command works like previously
    
    ## Checklist
    
    - [x] Unit tested

--- test_runner/src/main/kotlin/ftl/domain/DescribeAndroidModels.kt
+++ test_runner/src/main/kotlin/ftl/domain/DescribeAndroidModels.kt
@@ -1,19 +1,21 @@
 package ftl.domain
 
-import flank.common.logLn
 import ftl.api.fetchDeviceModelAndroid
 import ftl.args.AndroidArgs
-import ftl.environment.android.getDescription
+import ftl.presentation.Output
 import ftl.run.exception.FlankConfigurationError
+import ftl.run.exception.FlankGeneralError
 import java.nio.file.Paths
 
-interface DescribeAndroidModels {
+interface DescribeAndroidModels : Output {
     val configPath: String
     val modelId: String
 }
 
 operator fun DescribeAndroidModels.invoke() {
     if (modelId.isBlank()) throw FlankConfigurationError("Argument MODEL_ID must be specified.")
-    // TODO move getDescription() and printing presentation layer during refactor of presentation after #1728
-    logLn(fetchDeviceModelAndroid(AndroidArgs.loadOrDefault(Paths.get(configPath)).project).getDescription(modelId))
+    fetchDeviceModelAndroid(AndroidArgs.loadOrDefault(Paths.get(configPath)).project)
+        .find { it.id == modelId }
+        ?.out()
+        ?: throw FlankGeneralError("ERROR: '$modelId' is not a valid model")
 }
