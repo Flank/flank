1deaca00342c957de3e97e1b9b680ba70fd56075
pawelpasterz <32893017+pawelpasterz@users.noreply.github.com>
2021-05-07 15:33:29 +0200

    refactor: Logging refactor - android test environment (#1896)

---
+++ test_runner/src/main/kotlin/ftl/api/TestEnvironment.kt
@@ -0,0 +1,34 @@
+package ftl.api
+
+import com.google.testing.model.ProvidedSoftwareCatalog
+import ftl.adapter.FetchGoogleTestEnvironmentAndroid
+import ftl.adapter.FetchGoogleTestEnvironmentIos
+
+val fetchAndroidTestEnvironment: TestEnvironment.Android.Fetch get() = FetchGoogleTestEnvironmentAndroid
+val fetchIosTestEnvironment: TestEnvironment.Ios.Fetch get() = FetchGoogleTestEnvironmentIos
+
+object TestEnvironment {
+    data class Android(
+        val osVersions: List<OsVersion.Android>,
+        val models: List<DeviceModel.Android>,
+        val locales: List<Locale>,
+        val softwareCatalog: ProvidedSoftwareCatalog,
+        val networkProfiles: List<NetworkProfile>,
+        val orientations: List<Orientation>,
+        val ipBlocks: List<IpBlock>
+    ) {
+        interface Fetch : (String) -> Android
+    }
+
+    data class Ios(
+        val osVersions: List<OsVersion.Ios>,
+        val models: List<DeviceModel.Ios>,
+        val locales: List<Locale>,
+        val softwareCatalog: ProvidedSoftwareCatalog,
+        val networkProfiles: List<NetworkProfile>,
+        val orientations: List<Orientation>,
+        val ipBlocks: List<IpBlock>
+    ) {
+        interface Fetch : (String) -> Ios
+    }
+}
