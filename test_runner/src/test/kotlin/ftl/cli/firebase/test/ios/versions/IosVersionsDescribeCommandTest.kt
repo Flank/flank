e5373cd65c8e99e0925ecccdff0cd428d20a0ed1
Michael Wright <Slooxied@gmail.com>
2021-05-10 14:00:13 +0200

    Iteration 1

--- test_runner/src/test/kotlin/ftl/cli/firebase/test/ios/versions/IosVersionsDescribeCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/firebase/test/ios/versions/IosVersionsDescribeCommandTest.kt
@@ -1,11 +1,8 @@
 package ftl.cli.firebase.test.ios.versions
 
-import ftl.client.google.IosCatalog
 import ftl.presentation.cli.firebase.test.ios.versions.IosVersionsDescribeCommand
 import ftl.run.exception.FlankConfigurationError
 import ftl.test.util.TestHelper.getThrowable
-import io.mockk.mockkObject
-import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -15,14 +12,6 @@ import picocli.CommandLine
 class IosVersionsDescribeCommandTest {
     @get:Rule
     val systemOutRule: SystemOutRule = SystemOutRule().enableLog().muteForSuccessfulTests()
-
-    @Test
-    fun `should execute IosCatalog describeSoftwareVersion when run IosVersionsDescribeCommand`() {
-        mockkObject(IosCatalog) {
-            CommandLine(IosVersionsDescribeCommand()).execute("10.3")
-            verify { IosCatalog.describeSoftwareVersion(any(), any()) }
-        }
-    }
 
     @Test(expected = FlankConfigurationError::class)
     fun `should throw if version not specified`() {
