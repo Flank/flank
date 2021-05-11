1ed8fe613425dd570b0a7abe3061d4f5d7d80332
Pawel Pasterz <pawel.pasterz@gmail.com>
2021-05-11 18:02:52 +0200

    Refactor ip blocks output

--- test_runner/src/main/kotlin/ftl/adapter/google/IpBlockAdapter.kt
+++ test_runner/src/main/kotlin/ftl/adapter/google/IpBlockAdapter.kt
@@ -1,15 +1,17 @@
 package ftl.adapter.google
 
 import com.google.testing.model.DeviceIpBlock
-import ftl.api.IpBlock
+import ftl.api.IpBlockList
 import ftl.client.google.prettyDate
 
-internal fun List<DeviceIpBlock>.toApiModel(): List<IpBlock> = map { deviceIpBlock ->
-    IpBlock(
-        block = deviceIpBlock.block ?: UNABLE,
-        form = deviceIpBlock.form ?: UNABLE,
-        addedDate = deviceIpBlock.addedDate?.prettyDate() ?: UNABLE
-    )
-}
+internal fun List<DeviceIpBlock>.toApiModel(): IpBlockList = IpBlockList(
+    map { deviceIpBlock ->
+        IpBlockList.IpBlock(
+            block = deviceIpBlock.block ?: UNABLE,
+            form = deviceIpBlock.form ?: UNABLE,
+            addedDate = deviceIpBlock.addedDate?.prettyDate() ?: UNABLE
+        )
+    }
+)
 
 private const val UNABLE = "[Unable to fetch]"
