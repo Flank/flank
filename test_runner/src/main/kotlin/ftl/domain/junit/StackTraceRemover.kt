d4a337c116ca46ef61be1727cf8a088d29b14c16
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2021-05-13 14:07:20 +0200

    refactor: Add removing stack traces for simple report

--- test_runner/src/main/kotlin/ftl/domain/junit/StackTraceRemover.kt
+++ test_runner/src/main/kotlin/ftl/domain/junit/StackTraceRemover.kt
@@ -2,18 +2,14 @@ package ftl.domain.junit
 
 import ftl.api.JUnitTest
 
-fun JUnitTest.Result.removeStackTraces() = JUnitTest.Result(testsuites?.removeStackTraces())
+fun JUnitTest.Result.removeStackTraces() = copy(testsuites = testsuites?.removeStackTraces())
 
 private fun MutableCollection<JUnitTest.Suite>.removeStackTraces() = map { testSuite ->
-    testSuite.copy(testcases = testSuite.testcases?.withRemovedStackTraces()?.toMutableList())
+    testSuite.copy(testcases = testSuite.testcases?.withRemovedStackTraces())
 }.toMutableList()
 
 private fun MutableCollection<JUnitTest.Case>.withRemovedStackTraces() =
-    map { testCase -> testCase.removeFlakyStackTrace() }
+    map { testCase -> testCase.removeFlakyStackTrace() }.toMutableList()
 
-private fun JUnitTest.Case.removeFlakyStackTrace(): JUnitTest.Case = if (flaky == true) {
-    copy(
-        errors = null,
-        failures = null
-    )
-} else this
+private fun JUnitTest.Case.removeFlakyStackTrace(): JUnitTest.Case =
+    if (flaky == true) copy(errors = null, failures = null) else this
