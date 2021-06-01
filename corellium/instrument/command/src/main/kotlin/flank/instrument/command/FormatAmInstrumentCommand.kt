ba0570fe6e13a5730147cfca9eeb0fea31791277
Janek Góral <jan.goral@gogoapps.io>
2021-06-01 12:18:24 +0200

    Disable window animation in tests by default

--- corellium/instrument/command/src/main/kotlin/flank/instrument/command/FormatAmInstrumentCommand.kt
+++ corellium/instrument/command/src/main/kotlin/flank/instrument/command/FormatAmInstrumentCommand.kt
@@ -15,26 +15,34 @@ package flank.instrument.command
  * "package package.name"
  * ```
  *
- * @param packageName the package name of the android test apk.
- * @param testRunner the test runner full name.
- * @param testCases the list of test cases names.
+ * @param packageName The package name of the android test apk.
+ * @param testRunner The test runner full name.
+ * @param testCases The list of test cases names.
+ * @param noWindowAnimation Adds `--no-window-animation` to command.
  */
 fun formatAmInstrumentCommand(
     packageName: String,
     testRunner: String,
     testCases: List<String>,
+    noWindowAnimation: Boolean = true,
 ): String {
     val testCasesChunk = testCases // example: listOf("class foo.Bar#baz")
         .map { it.split(" ") } // example: listOf(listOf("class", "foo.Bar#baz"))
         .groupBy({ it.first() }, { it.last() }) // example: first => "class", last => "foo.Bar#baz"
         .toList().joinToString("") { (type, tests: List<String>) ->
             "-e $type ${tests.joinToString(",")} " // example: "-e class foo.Bar#baz"
-        } // example: "-e class foo.Bar#baz1,foo.Bar#baz2 -e package foo.test "
+        }.trimEnd() // example: "-e class foo.Bar#baz1,foo.Bar#baz2 -e package foo.test"
 
     val runnerChunk = "$packageName/$testRunner"
 
-    // example: "am instrument -r -w -e class foo.Bar#baz foo.test/androidx.test.runner.AndroidJUnitRunner"
-    return AM_INSTRUMENT + testCasesChunk + runnerChunk
+    // example: "am instrument -r -w --no-window-animation -e class foo.Bar#baz foo.test/androidx.test.runner.AndroidJUnitRunner"
+    return listOfNotNull(
+        AM_INSTRUMENT,
+        NO_WINDOW_ANIMATION.takeIf { noWindowAnimation },
+        testCasesChunk,
+        runnerChunk
+    ).joinToString(" ")
 }
 
-private const val AM_INSTRUMENT = "am instrument -r -w "
+private const val AM_INSTRUMENT = "am instrument -r -w"
+private const val NO_WINDOW_ANIMATION = "--no-window-animation"
