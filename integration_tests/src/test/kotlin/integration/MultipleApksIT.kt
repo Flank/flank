d1270daa7cf271101f3e6d683e74099301f625f5
Pawel Pasterz <pawel.pasterz@gmail.com>
2021-06-04 09:54:08 +0200

    Update IT

--- integration_tests/src/test/kotlin/integration/MultipleApksIT.kt
+++ integration_tests/src/test/kotlin/integration/MultipleApksIT.kt
@@ -20,9 +20,9 @@ import utils.assertTestResultContainsWebLinks
 import utils.findTestDirectoryFromOutput
 import utils.json
 import utils.loadAsTestSuite
-import utils.multipleFailedTests
 import utils.multipleSuccessfulTests
 import utils.removeUnicode
+import utils.testResults.TestSuite
 import utils.toJUnitXmlFile
 import utils.toOutputReportFile
 
@@ -53,12 +53,16 @@ class MultipleApksIT {
 
         xmlResult.assertTestResultContainsWebLinks()
         xmlResult.assertTestPass(multipleSuccessfulTests)
-        xmlResult.assertTestFail(multipleFailedTests)
+        xmlResult.assertTestFail(listOf("test2"))
 
-        xmlResult.testSuites.groupBy { it.name }.run {
-            assertEquals(2, get("NexusLowRes-28-en-portrait")?.size)
-            assertEquals(9, get("Pixel2-28-en-portrait")?.size)
-            assertEquals(1, get("Nexus6P-28-en-portrait")?.size)
+        xmlResult.testSuites.groupBy { it.name }.mapValues { it.value.flatMap(TestSuite::testCases) }.run {
+            assertEquals(20, get("NexusLowRes-28-en-portrait")?.size)
+            assertEquals(1, get("Pixel2-28-en-portrait")?.size)
+            assertEquals("com.example.test_app.InstrumentedTest", get("Pixel2-28-en-portrait")?.get(0)?.classname)
+            assertEquals("test2", get("Pixel2-28-en-portrait")?.get(0)?.name)
+            assertEquals(1, get("Nexus6P-27-en-portrait")?.size)
+            assertEquals("com.example.test_app.InstrumentedTest", get("Nexus6P-27-en-portrait")?.get(0)?.classname)
+            assertEquals("test", get("Nexus6P-27-en-portrait")?.get(0)?.name)
         }
 
         val outputReport = resOutput.findTestDirectoryFromOutput().toOutputReportFile().json().asOutputReport()
@@ -76,7 +80,7 @@ class MultipleApksIT {
             .map { it.testAxises }
             .flatten()
 
-        assertThat(testsResults.sumOf { it.suiteOverview.failures }).isEqualTo(5)
-        assertThat(testsResults.sumOf { it.suiteOverview.total }).isEqualTo(41)
+        assertThat(testsResults.sumOf { it.suiteOverview.failures }).isEqualTo(1)
+        assertThat(testsResults.sumOf { it.suiteOverview.total }).isEqualTo(22)
     }
 }
