Flank/flank

commit ed993ed3506722a544e7dd27f12609a1873b4093

Fix bucket list error (#531)
* Fix bucket list error

* Update gradle

test_runner/src/main/kotlin/ftl/args/ArgsHelper.kt
@@ -24,7 +24,6 @@ import ftl.shard.Shard
 import ftl.shard.StringShards
 import ftl.shard.stringShards
 import ftl.util.Utils
-import ftl.util.Utils.fatalError
 import java.io.File
 import java.net.URI
 import java.nio.file.Files
@@ -103,11 +102,14 @@ object ArgsHelper {
     fun createJunitBucket(projectId: String, junitGcsPath: String) {
         if (FtlConstants.useMock || junitGcsPath.isEmpty()) return
         val bucket = junitGcsPath.drop(GCS_PREFIX.length).substringBefore('/')
-            createGcsBucket(projectId, bucket)
+        createGcsBucket(projectId, bucket)
     }
 
+    // Make best effort to list/create the bucket.
+    // Due to permission issues, the user may not be able to list or create buckets.
     fun createGcsBucket(projectId: String, bucket: String): String {
-        if (bucket.isEmpty()) return GcToolResults.getDefaultBucket(projectId) ?: throw RuntimeException("Failed to make bucket for $projectId")
+        if (bucket.isEmpty()) return GcToolResults.getDefaultBucket(projectId)
+            ?: throw RuntimeException("Failed to make bucket for $projectId")
         if (useMock) return bucket
 
         // test lab supports using a special free storage bucket
@@ -122,21 +124,29 @@ object ArgsHelper {
         val storageLocation = "us-central1"
 
         val bucketListOption = Storage.BucketListOption.prefix(bucket)
-        val storageList = storage.list(bucketListOption).values?.map { it.name } ?: emptyList()
+        val storageList = emptyList<String>()
+
+        try {
+            storage.list(bucketListOption).values?.map { it.name }
+        } catch (e: Exception) {
+            // User may not have list permission
+        }
+
         if (storageList.contains(bucket)) return bucket
 
         try {
-            return storage.create(
+            storage.create(
                 BucketInfo.newBuilder(bucket)
                     .setStorageClass(StorageClass.REGIONAL)
                     .setLocation(storageLocation)
                     .setLabels(bucketLabel)
                     .build()
-            ).name
-        } catch (e: com.google.cloud.storage.StorageException) {
-            fatalError("Failed to create bucket $bucket\n${e.message}")
-            throw RuntimeException("will not throw")
+            )
+        } catch (e: Exception) {
+            // User may not have create permission
         }
+
+        return bucket
     }
 
     private fun serviceAccountProjectId(): String? {

test_runner/gradle/wrapper/gradle-wrapper.properties
@@ -1,5 +1,5 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-5.1-all.zip
+distributionUrl=https\://services.gradle.org/distributions/gradle-5.3-all.zip
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists

commit 9b780629bf5cf3e55f4d69ac9aa806a6949b811a

Fix: bucket not found but can't be created (#2005)
Flank was showing:
> Warning: Failed to make bucket for ...
> Cause: Sorry, that name is not available. Please try a different one.

It didn't find the bucket but it was there, the reason is the list of buckets was never set.

It's been broken since: https://github.com/Flank/flank/pull/531/files#diff-81920894b2478868a06b5f2210f003afaf3ca682d7cb61afe1c8dec2cecdb191R127

## Test Plan
Tested manually but looking if on how to write an automated test

## Checklist

- [ ] Documented
- [ ] Unit tested
- [ ] Integration tests updated


I have read the CLA Document and I hereby sign the CLA

test_runner/src/main/kotlin/ftl/args/ArgsHelper.kt
@@ -158,12 +158,11 @@ object ArgsHelper {
         val storageLocation = "us-central1"
 
         val bucketListOption = Storage.BucketListOption.prefix(bucket)
-        val storageList = emptyList<String>()
-
-        try {
-            storage.list(bucketListOption).values?.map { it.name }
+        val storageList = try {
+            storage.list(bucketListOption).values?.map { it.name } ?: emptyList()
         } catch (e: Exception) {
             // User may not have list permission
+            emptyList()
         }
 
         if (storageList.contains(bucket)) return bucket