6d50223acffa1ec7b45709b43f859cc7af219213
Janek Góral <jan.goral@gogoapps.io>
2021-06-11 11:38:00 +0200

    Add example graph

--- tool/execution/parallel/src/main/kotlin/flank/exection/parallel/Reduce.kt
+++ tool/execution/parallel/src/main/kotlin/flank/exection/parallel/Reduce.kt
@@ -5,7 +5,7 @@ import flank.exection.parallel.internal.reduceTo
 
 /**
  * Reduce given [Tasks] by [select] types to remove unneeded tasks from the graph.
- * The returned graph will only tasks that are returning selected types, their dependencies and derived dependencies.
+ * The returned graph will hold only tasks that are returning selected types, their dependencies and derived dependencies.
  * Additionally this is keeping also the validators for initial state.
  *
  * @return Reduced [Tasks]
