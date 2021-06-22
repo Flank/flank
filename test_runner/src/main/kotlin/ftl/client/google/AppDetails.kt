a755a61859cb8fc0c49163186668f54f26ee0bd2
Adam <adam.filipowicz92@gmail.com>
2021-06-22 16:56:56 +0200

    Update AppDetails.kt

--- test_runner/src/main/kotlin/ftl/client/google/AppDetails.kt
+++ test_runner/src/main/kotlin/ftl/client/google/AppDetails.kt
@@ -5,5 +5,3 @@ import com.google.testing.model.FileReference
 fun getAndroidAppDetails(gcsAppPath: String): String =
     GcTesting.get.ApplicationDetailService().getApkDetails(FileReference().apply { gcsPath = gcsAppPath })
         .execute()?.apkDetail?.apkManifest?.packageName?.toString().orEmpty()
-
-fun getIosAppDetails(gcsAppPath: String): String = GcTesting.get.applicationName
