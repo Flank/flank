68e02f4e305f393c97b55e889fb9be2b05a38ba7
Jan Góral <60390247+jan-gogo@users.noreply.github.com>
2021-07-12 09:56:08 +0200

    refactor: Move Config.kt into :tool:config module. (#2079)
    Just adds module `:tool:config` and moves `Config.kt` for making CLI not defining any tool by itself.

--- tool/config/src/main/kotlin/flank/config/Config.kt
+++ tool/config/src/main/kotlin/flank/config/Config.kt
@@ -1,4 +1,4 @@
-package flank.corellium.cli.util
+package flank.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
@@ -8,7 +8,7 @@ import java.io.File
 /**
  * Abstraction for configuration structures
  */
-internal interface ConfigMap {
+interface ConfigMap {
     val data: MutableMap<String, Any?>
 }
 
@@ -20,21 +20,21 @@ internal interface ConfigMap {
  * @param others Other configuration to accumulate in first.
  * @return The [first] parameter with accumulated values.
  */
-internal fun <C : ConfigMap> merge(first: C, vararg others: C): C =
+fun <C : ConfigMap> merge(first: C, vararg others: C): C =
     others.fold(first) { acc, config -> acc.apply { data += config.data } }
 
 /**
  * Factory method for the empty configuration map with null as default value
  */
-internal fun emptyConfigMap(): MutableMap<String, Any?> =
+fun emptyConfigMap(): MutableMap<String, Any?> =
     mutableMapOf<String, Any?>().withDefault { null }
 
 /**
  * Load the yaml configuration file as structure.
  */
-internal inline fun <reified T> loadYaml(path: String): T =
+inline fun <reified T> loadYaml(path: String): T =
     yamlMapper.readValue(File(path), T::class.java)
 
-private val yamlMapper: ObjectMapper by lazy {
+val yamlMapper: ObjectMapper by lazy {
     ObjectMapper(YAMLFactory()).registerKotlinModule()
 }
