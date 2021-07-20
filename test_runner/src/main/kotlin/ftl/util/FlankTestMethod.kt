fd1f5c64760232a6d24094416ad48da4e3066338
Piotr Adamczyk <piotr.adamczyk@gogoapps.io>
2021-07-20 22:20:19 +0200

    feat: Added Sdk Suppress Handling

--- test_runner/src/main/kotlin/ftl/util/FlankTestMethod.kt
+++ test_runner/src/main/kotlin/ftl/util/FlankTestMethod.kt
@@ -1,7 +1,10 @@
 package ftl.util
 
+import ftl.run.platform.android.SdkSuppressLevels
+
 data class FlankTestMethod(
     val testName: String,
     val ignored: Boolean = false,
-    val isParameterizedClass: Boolean = false
+    val isParameterizedClass: Boolean = false,
+    val sdkSuppressLevels: SdkSuppressLevels? = null
 )
