fc7175b6d9f2cc5ec3d41e42c41aaf5c7ecc281b
Jan Góral <60390247+jan-gogo@users.noreply.github.com>
2021-07-23 14:32:54 +0200

    * Fix parseJUnitTestResults function (#2097)
    * Fix mergeDurations function

--- tool/junit/src/main/kotlin/flank/junit/internal/ParseJUnitTestResults.kt
+++ tool/junit/src/main/kotlin/flank/junit/internal/ParseJUnitTestResults.kt
@@ -9,7 +9,9 @@ internal val parseJUnitTestResults = JUnit.TestResult.Parse { path ->
     val file = File(path)
     when {
         file.isFile -> sequenceOf(file)
-        file.isDirectory -> file.walkTopDown().filter { next -> next.name == JUnit.REPORT_FILE_NAME }
+        file.isDirectory -> file.walkBottomUp()
+            .filter { next -> next.name == JUnit.REPORT_FILE_NAME }
+            .sortedByDescending(File::lastModified)
         else -> emptySequence()
     }.map { next: File ->
         next.reader().parseJUnitReport().testsuites.mapToTestResults()
