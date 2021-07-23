b501d1333310866db601a80a5aca0c3737814b9c
Janek Góral <jan.goral@gogoapps.io>
2021-07-23 13:48:10 +0200

    * Expose function for wrapping state into context in ContextProvider. * Add generic function for creating state property which static type checking. * Improve DeadlockError message. * Remove redundant data value from DependenciesError messages.

--- tool/execution/parallel/src/main/kotlin/flank/exection/parallel/internal/ContextProvider.kt
+++ tool/execution/parallel/src/main/kotlin/flank/exection/parallel/internal/ContextProvider.kt
@@ -2,16 +2,21 @@ package flank.exection.parallel.internal
 
 import flank.exection.parallel.ExecuteTask
 import flank.exection.parallel.Parallel
+import flank.exection.parallel.ParallelState
 import flank.exection.parallel.validator
 
 /**
- * Abstract factory for creating task function.
+ * Abstract factory for wrapping state into context and creating task function.
  */
-abstract class ContextProvider<X : Parallel.Context> internal constructor() {
+abstract class ContextProvider<X : Parallel.Context>
+internal constructor() : (ParallelState) -> X {
     protected abstract val context: () -> X
 
-    operator fun <R> invoke(body: suspend X.() -> R): ExecuteTask<R> =
-        { context().also { it.state = this }.body() }
+    override fun invoke(state: ParallelState): X =
+        context().also { it.state = state }
+
+    operator fun <R> invoke(body: suspend X.(ParallelState) -> R): ExecuteTask<R> =
+        { invoke(this).body(this) }
 
     val validate by lazy { validator(context) }
 }
