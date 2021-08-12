af4f65b8a4115d4c18003b90ee0a92b980791c96
Janek Góral <jan.goral@gogoapps.io>
2021-08-12 14:01:01 +0200

    Add issue reference

--- corellium/domain/src/main/kotlin/flank/corellium/domain/test/android/task/FetchDeviceCost.kt
+++ corellium/domain/src/main/kotlin/flank/corellium/domain/test/android/task/FetchDeviceCost.kt
@@ -4,4 +4,5 @@ import flank.corellium.domain.TestAndroid.DeviceCostPerSecond
 import flank.exection.parallel.using
 
 // TODO fetch this value from corellium API.
+// https://github.com/Flank/flank/issues/2133
 val fetchDeviceCostPerSecond = DeviceCostPerSecond using { 0L }
