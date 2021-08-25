bfe8d5465a7f7eaba3a392a72e23eaf1a749fbdf
piotradamczyk5 <65554637+piotradamczyk5@users.noreply.github.com>
2021-08-25 21:50:18 +0200

    fix: Merging results for non legacy Android run (#2151)
    Fixes #2143
    
    ## Test Plan
    > How do we know the code works?
    
    Results are merged properly as per discussion.
    
    1. Run flank with `flank-multiple-success.yml` config
    2. Check that JUnitReport.xml has grouped `testsuite` node by name
    
    
    Before:
    ```xml
    <?xml version='1.0' encoding='UTF-8' ?>
    <testsuites>
      <testsuite name="NexusLowRes-28-en-portrait" tests="8" failures="0" flakes="0" errors="0" skipped="0" time="3.364" timestamp="2021-08-24T20:39:40" hostname="localhost">
        ...
      </testsuite>
      <testsuite name="NexusLowRes-28-en-portrait" tests="12" failures="0" flakes="0" errors="0" skipped="0" time="3.853" timestamp="2021-08-24T20:39:40" hostname="localhost">
        ...
      </testsuite>
      <testsuite name="junit-ignored" tests="4" failures="0" errors="0" skipped="4" time="0.0" timestamp="2021-08-24T20:40:30" hostname="localhost">
        ...
      </testsuite>
    </testsuites>
    
    ```
    
    After:
    ```xml
    <?xml version='1.0' encoding='UTF-8' ?>
    <testsuites>
      <testsuite name="NexusLowRes-28-en-portrait#" tests="20" failures="0" errors="0" skipped="0" time="6.894" timestamp="2021-08-24T20:47:28" hostname="localhost">
        ...
      </testsuite>
      <testsuite name="junit-ignored" tests="4" failures="0" errors="0" skipped="4" time="0.0" timestamp="2021-08-24T20:48:17" hostname="localhost">
      ...
      </testsuite>
    </testsuites>
    
    ```
    
    ## Checklist
    
    - [ ] Documented
    - [ ] Unit tested
    - [ ] Integration tests updated

--- test_runner/src/main/kotlin/ftl/client/junit/CreateJUnitTestResult.kt
+++ test_runner/src/main/kotlin/ftl/client/junit/CreateJUnitTestResult.kt
@@ -2,13 +2,19 @@ package ftl.client.junit
 
 import com.google.testing.model.TestExecution
 import ftl.api.JUnitTest
+import ftl.domain.junit.merge
 
-internal fun List<TestExecution>.createJUnitTestResult() = JUnitTest.Result(
+internal fun List<TestExecution>.createJUnitTestResult(): JUnitTest.Result = JUnitTest.Result(
     testsuites = filterNullToolResultsStep()
         .createTestExecutionDataListAsync()
         .prepareForJUnitResult()
         .createJUnitTestSuites()
+        .mergeResults()
         .toMutableList()
 )
+
+private fun List<JUnitTest.Suite>.mergeResults(): List<JUnitTest.Suite> = groupBy { it.name }
+    .values
+    .map { it.reduce { acc, suite -> acc.merge(suite) } }
 
 private fun List<TestExecution>.filterNullToolResultsStep() = filter { it.toolResultsStep != null }
