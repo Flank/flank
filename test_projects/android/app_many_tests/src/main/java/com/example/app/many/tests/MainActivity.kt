cc6cf9ea5dacd6119d1bbeb150f5787c971c7757
bootstraponline <code@bootstraponline.com>
2021-11-15 17:45:07 -0500

    test: Update app_many_tests to AndroidX libraries (#2185)
    FTL is deprecating support for the Android Test Orchestrator that is
    part of the legacy/deprecated Android Test Support libraries.
    
    This CL updates the app_many_tests test app to the new AndroidX
    libraries, as this app is used in integration tests that enable Test
    Orchestrator.
    
    Note, users can still use Android Support and Android Test Support
    libraries. They just cannot use the legacy Test Orchestrator.
    
    Co-authored-by: Michael Zoech <michi.zoech@gmail.com>

--- test_projects/android/app_many_tests/src/main/java/com/example/app/many/tests/MainActivity.kt
+++ test_projects/android/app_many_tests/src/main/java/com/example/app/many/tests/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.app.many.tests
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
