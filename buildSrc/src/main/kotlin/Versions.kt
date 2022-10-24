2d7f6537142fbf16cd9c964362013a6ba1a34763
tonybaroneee <tonybaroneee@users.noreply.github.com>
2022-10-24 06:18:18 +0000

    [Automatic PR] Dependencies update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     // https://github.com/3breadt/dd-plist/releases
-    const val DD_PLIST = "1.23"
+    const val DD_PLIST = "1.25"
 
     // https://github.com/jeremymailen/kotlinter-gradle
     const val KTLINT_GRADLE = "3.9.0"
@@ -23,34 +23,34 @@ object Versions {
     const val KOTLIN = "1.6.0"
 
     // https://github.com/Kotlin/kotlinx.coroutines/releases
-    const val KOTLIN_COROUTINES = "1.6.3"
+    const val KOTLIN_COROUTINES = "1.6.4"
 
     // https://github.com/remkop/picocli/releases
     const val PICOCLI = "4.6.3"
 
     // https://search.maven.org/search?q=a:google-api-services-toolresults%20g:com.google.apis
-    const val GOOGLE_API_TOOLRESULTS = "v1beta3-rev20210809-1.32.1"
+    const val GOOGLE_API_TOOLRESULTS = "v1beta3-rev20220808-2.0.0"
 
     // https://mvnrepository.com/artifact/com.google.api-client/google-api-client
-    const val GOOGLE_API = "1.35.2"
+    const val GOOGLE_API = "2.0.0"
 
     // https://github.com/googleapis/google-auth-library-java/releases
     // NOTE: https://github.com/googleapis/google-oauth-java-client is End of Life and replaced by google-auth-library-java
     // https://github.com/googleapis/google-oauth-java-client/issues/251#issuecomment-504565533
-    const val GOOGLE_AUTH = "1.8.0"
+    const val GOOGLE_AUTH = "1.12.1"
 
     // https://search.maven.org/search?q=a:google-cloud-nio%20g:com.google.cloud
-    const val GOOGLE_NIO = "0.124.7"
+    const val GOOGLE_NIO = "0.124.18"
 
     // https://search.maven.org/search?q=a:google-cloud-storage%20g:com.google.cloud
-    const val GOOGLE_STORAGE = "2.9.3"
+    const val GOOGLE_STORAGE = "2.13.1"
 
     // https://github.com/google/gson/releases
-    const val GSON = "2.9.0"
+    const val GSON = "2.9.1"
 
     // https://github.com/FasterXML/jackson-core/releases
     // https://github.com/FasterXML/jackson-dataformat-xml/releases
-    const val JACKSON = "2.13.3"
+    const val JACKSON = "2.13.4"
 
     const val JUNIT = "4.13.2"
 
@@ -58,10 +58,10 @@ object Versions {
     const val JSOUP = "1.13.1"
 
     // https://github.com/ktorio/ktor/releases
-    const val KTOR = "2.0.3"
+    const val KTOR = "2.1.2"
 
     // https://github.com/qos-ch/logback/releases
-    const val LOGBACK = "1.2.11"
+    const val LOGBACK = "1.4.4"
 
     // https://github.com/square/okhttp/releases
     const val OKHTTP = "4.9.1"
@@ -75,13 +75,13 @@ object Versions {
     // https://github.com/FasterXML/woodstox/releases
     const val WOODSTOX = "6.2.4"
 
-    const val KOTLIN_LOGGING = "2.1.23"
+    const val KOTLIN_LOGGING = "3.0.2"
 
     // https://github.com/mockk/mockk
-    const val MOCKK = "1.12.4"
+    const val MOCKK = "1.13.2"
 
     // https://commons.apache.org/proper/commons-text/
-    const val COMMON_TEXT = "1.9"
+    const val COMMON_TEXT = "1.10.0"
 
     // https://github.com/jboss-logging/commons-logging-jboss-logging
     // JBoss logging dep is used by the Google Auth library when configuring a proxy.
@@ -92,21 +92,21 @@ object Versions {
     const val JANSI = "2.4.0"
 
     // https://github.com/ben-manes/gradle-versions-plugin/releases
-    const val BEN_MANES = "0.42.0"
+    const val BEN_MANES = "0.43.0"
 
     // https://github.com/Guardsquare/proguard
     const val PROGUARD = "7.2.2"
 
     // https://mvnrepository.com/artifact/org.json/json
-    const val JSON = "20220320"
+    const val JSON = "20220924"
 
     // ============== flank-scripts ==============
-    const val KOTLIN_SERIALIZATION = "1.3.3"
+    const val KOTLIN_SERIALIZATION = "1.4.1"
     const val ARCHIVE_LIB = "1.2.0"
     const val TUKAANI_XZ = "1.9"
     const val FUEL = "2.3.1"
     const val CLIKT = "2.8.0"
-    const val JCABI_GITHUB = "1.2.0"
-    const val SLF4J_NOP = "1.7.36"
+    const val JCABI_GITHUB = "1.3.2"
+    const val SLF4J_NOP = "2.0.3"
     const val GLASSFISH_JSON = "1.1.4"
 }
