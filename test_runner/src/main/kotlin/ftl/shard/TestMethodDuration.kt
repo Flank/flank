159fc9f72f70a28bf5c09bde5d3235e4e25ada36
Sinan Kozak <kozaxinan@gmail.com>
2023-04-28 11:02:59 +0200

    Revert "Shard calculation should not use skipped tests time"
    This reverts commit c68958ba059cf8f71a9d3c2950399638a2156a8f.

--- test_runner/src/main/kotlin/ftl/shard/TestMethodDuration.kt
+++ test_runner/src/main/kotlin/ftl/shard/TestMethodDuration.kt
@@ -4,7 +4,6 @@ import ftl.api.JUnitTest
 import ftl.args.AndroidArgs
 import ftl.args.IArgs
 import ftl.domain.junit.empty
-import ftl.domain.junit.skipped
 
 fun createTestMethodDurationMap(junitResult: JUnitTest.Result, args: IArgs): Map<String, Double> {
     val junitMap = mutableMapOf<String, Double>()
@@ -13,7 +12,7 @@ fun createTestMethodDurationMap(junitResult: JUnitTest.Result, args: IArgs): Map
 
     junitResult.testsuites?.forEach { testsuite ->
         testsuite.testcases?.forEach { testcase ->
-            if (!testcase.empty() && testcase.time != null && !testcase.skipped()) {
+            if (!testcase.empty() && testcase.time != null) {
                 val key = if (args is AndroidArgs) testcase.androidKey() else testcase.iosKey()
                 val time = testcase.time.toDouble()
                 if (time >= 0) junitMap[key] = time
