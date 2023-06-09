af9fded8f03735b54aaabd404a45e843d78b9669
Adam Duke <94930+adamvduke@users.noreply.github.com>
2023-06-09 14:08:23 -0400

    fix: update google-auth-library-oauth2-http version to 1.17.0 (#2375)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -40,7 +40,7 @@ object Versions {
     // https://github.com/googleapis/google-auth-library-java/releases
     // NOTE: https://github.com/googleapis/google-oauth-java-client is End of Life and replaced by google-auth-library-java
     // https://github.com/googleapis/google-oauth-java-client/issues/251#issuecomment-504565533
-    const val GOOGLE_AUTH = "1.8.0"
+    const val GOOGLE_AUTH = "1.17.0"
 
     // https://search.maven.org/search?q=a:google-cloud-nio%20g:com.google.cloud
     const val GOOGLE_NIO = "0.124.7"
