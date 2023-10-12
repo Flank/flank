2534fa5aab5f9a646204370ffd194eb6131028b1
Cleo Schneider <cleoschneider@google.com>
2023-10-12 14:10:07 -0400

    Replace hub with GitHub CLI 'gh' for GitHub Actions

--- flank-scripts/src/main/kotlin/flank/scripts/ops/github/DeleteOldRelease.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/ops/github/DeleteOldRelease.kt
@@ -4,4 +4,4 @@ import flank.scripts.utils.runCommand
 
 fun deleteOldRelease(tag: String) = "$DELETE_RELEASE_COMMAND $tag".runCommand()
 
-private const val DELETE_RELEASE_COMMAND = "hub release delete"
+private const val DELETE_RELEASE_COMMAND = "gh release delete"
