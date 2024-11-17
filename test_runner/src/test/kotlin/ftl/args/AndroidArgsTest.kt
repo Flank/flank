136078bc8687a8c28527212746423ef1b4a6601b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-11-16 23:24:30 -0500

    spelling: parameterized
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test_runner/src/test/kotlin/ftl/args/AndroidArgsTest.kt
+++ test_runner/src/test/kotlin/ftl/args/AndroidArgsTest.kt
@@ -2828,7 +2828,7 @@ AndroidArgs
     }
 
     @Test
-    fun `should throw exception if paramterized test is incorrect`() {
+    fun `should throw exception if parameterized test is incorrect`() {
         val yaml = """
         gcloud:
             # Android gcloud
@@ -2855,7 +2855,7 @@ AndroidArgs
     }
 
     @Test
-    fun `should throw NOT throw an exception if paramterized test is ignore-all`() {
+    fun `should throw NOT throw an exception if parameterized test is ignore-all`() {
         val yaml = """
         gcloud:
             # Android gcloud
@@ -2867,7 +2867,7 @@ AndroidArgs
     }
 
     @Test
-    fun `should throw NOT throw an exception if paramterized test is shard-into-single`() {
+    fun `should throw NOT throw an exception if parameterized test is shard-into-single`() {
         val yaml = """
         gcloud:
             # Android gcloud
